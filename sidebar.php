3ffc45401627f552564cb44eeafa25ef139e1a18
Chris Bishop <domingobishop@gmail.com>
2015-12-30 15:58:01 +0000

    sidebar file added

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<aside id="secondary" class="sidebar" role="complementary">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<p>SIDEBAR</p>
+			</div>
+		</div>
+	</div>
+</aside>
--- single.php
+++ single.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?>
 
 <main id="content" role="main">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<p>SINGLE</p>
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
+	<article id="primary" class="content-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p>PAGE</p>
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	</article>
+	<?php get_sidebar(); ?>
 </main>
 
 <?php get_footer(); ?>
