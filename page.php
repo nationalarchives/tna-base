6f37dad175e451d4dd9be1534b2dc1cc437645c2
Chris Bishop <domingobishop@gmail.com>
2015-12-30 17:22:34 +0000

    added post IDs

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-	<div id="primary">
+	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row">
-				<main id="content" class="content-area col-xs-12 col-sm-12 col-md-8 col-lg-8" role="main">
+				<main id="main" class="col-xs-12 col-sm-12 col-md-8" role="main">
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 						<p>PAGE</p>
 						<h1><?php the_title(); ?></h1>
--- single.php
+++ single.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-	<main id="content" <?php post_class(); ?>>
+	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row">
-				<article id="primary" class="content-area col-xs-12 col-sm-12 col-md-8 col-lg-8" role="main">
-					<p>SINGLE</p>
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				</article>
+				<main id="main" class="col-xs-12 col-sm-12 col-md-8" role="main">
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<p>SINGLE</p>
+						<h1><?php the_title(); ?></h1>
+						<?php the_content(); ?>
+					</article>
+				</main>
 				<?php get_sidebar(); ?>
 			</div>
 		</div>
-	</main>
+	</div>
 
 <?php get_footer(); ?>
