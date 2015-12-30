062ce5203d594c434b43de6a0e922f22b1b77c29
Chris Bishop <domingobishop@gmail.com>
2015-12-30 16:21:10 +0000

    responsive classes

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 	<main id="content" role="main">
 		<div class="container">
 			<div class="row">
-				<article id="primary" class="content-area col-lg-8">
+				<article id="primary" class="content-area col-xs-12 col-sm-12 col-md-8 col-lg-8">
 					<p>PAGE</p>
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,3 @@
-<aside id="secondary" class="sidebar col-lg-4" role="complementary">
+<aside id="secondary" class="sidebar col-xs-12 col-sm-12 col-md-4 col-lg-4" role="complementary">
 				<p>SIDEBAR</p>
 </aside>
