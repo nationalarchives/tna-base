4b2e59f5222ce6d1a58682268f52086f96cf9c33
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2015-12-31 10:50:57 +0000

    Footer update

---
+++ _content/footer-content.php
@@ -0,0 +1,71 @@
+<div class="row">
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <h4>The National Archives</h4>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <h4>Find out more</h4>
+        <ul>
+            <li><a href="<?php echo network_site_url(); ?>/contact/">Contact us</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/pressroom/">Press room</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/jobs/default.htm">Jobs and careers</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/get-involved/friends.htm">Friends of The National Archives</a></li>
+        </ul>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <h4>Websites</h4>
+        <ul>
+            <li><a href="http://blog.nationalarchives.gov.uk/">Blog</a></li>
+            <li><a href="http://media.nationalarchives.gov.uk/">Podcasts and videos</a></li>
+            <li><a href="http://labs.nationalarchives.gov.uk/wordpress/">Labs</a></li>
+            <li><a href="https://community.nationalarchives.gov.uk/">Community</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/webarchive/">UK Government Web Archive</a></li>
+            <li><a href="http://www.legislation.gov.uk/" target="_blank">Legislation.gov.uk</a></li>
+            <li><a href="https://www.thegazette.co.uk/" target="_blank">The Gazette</a></li>
+        </ul>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <h4>Follow us</h4>
+        <ul>
+            <li><a href="https://twitter.com/@UKNatArchives" target="_blank">Twitter</a></li>
+            <li><a href="https://www.youtube.com/user/NationalArchives08" target="_blank">YouTube</a></li>
+            <li><a href="https://www.flickr.com/photos/nationalarchives" target="_blank">Flickr</a></li>
+            <li><a href="https://www.facebook.com/TheNationalArchives?_rdr=p" target="_blank" >Facebook</a></li>
+            <li><a href="https://commons.wikimedia.org/wiki/Commons:The_National_Archives_(United_Kingdom)" target="_blank">Wikimedia Commons</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/rss/">RSS</a></li>
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3 col-md-offset-3">
+        <h4>Site help</h4>
+        <ul>
+            <li><a href="<?php echo network_site_url(); ?>/help/">Help</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/help/faqs.htm">FAQs</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/help/atoz.htm">Website A-Z index</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/help/accessibility.htm">Accessibility</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/help/sitemap.htm">Site map</a></li>
+        </ul>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+        <h4>Legal</h4>
+        <ul>
+            <li><a href="<?php echo network_site_url(); ?>/legal/">Terms of use</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/legal/privacy.htm">Privacy policy</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/legal/cookies.htm">Cookies</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/foi/default.htm">Freedom of Information</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/about/our-role/transparency/">Transparency</a></li>
+            <li><a href="<?php echo network_site_url(); ?>/legal/our-fees.htm">Our fees</a></li>
+        </ul>
+    </div>
+    <div class="footer-col col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <h4>Featured Flickr image</h4>
+            <p>West End London street scene, by Grace Golden (<a href="https://www.flickr.com/photos/nationalarchives/3047452739/" title="External website - opens in new window" target="_blank">INF 3/1738</a>)</p>
+            <a target="_blank" title="External website - opens in a new window" href="https://www.flickr.com/photos/nationalarchives/3047452739/"> <img class="img-responsive" src="https://www.nationalarchives.gov.uk/images/global/inf-3-1738-grace-golden.jpg" alt="West End London street scene, by Grace Golden"></a>
+
+    </div>
+</div>
+<div class="row footer-base">
+    <div class="col-lg-12">
+        <p>OGL info and logos</p>
+    </div>
+</div>
