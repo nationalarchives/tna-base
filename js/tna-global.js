997defa3bb4232ecb1f5df28c43ac1477f22833f
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-01-05 15:04:27 +0000

    Cookie policy

--- js/tna-global.js
+++ js/tna-global.js
@@ -2,8 +2,13 @@
  * TNA - Global js
  */
 
-// 1    $.backToTopLink() Displays a back to top link when the user has scrolled on longer pages.
-//      Defaults are provided but can be overridden with options argument (object)
+// ----------------------------------------
+// 1. Back to top
+// 2. Website cookie
+// ----------------------------------------
+
+// 1. Back to top  - Displays a back to top link when the user has scrolled on longer pages.
+//    Defaults are provided but can be overridden with options argument (object)
 $(document).ready(function(){
     // Check the position of the page
     $(window).scroll(function(){
@@ -19,4 +24,52 @@ $(document).ready(function(){
         return false;
     });
 });
+// ----------------------------------------
 
+// ----------------------------------------
+// 2 Website Cookie -----------------------
+// ----------------------------------------
+// 2.1 setThisCookie()
+
+tnaSetThisCookie = function (name, days) {
+    var d = new Date();
+    d.setTime(d.getTime() + 1000 * 60 * 60 * 24 * days);
+    document.cookie = name + "=true;path=/;expires=" + d.toGMTString() + ';';
+};
+// 2.2 checkForThisCookie()
+tnaCheckForThisCookie = function (name) {
+    if (document.cookie.indexOf(name) === -1) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+// 2.3 Cookie notification
+$(function(){ // All content must be placed within this IIFE.
+    if (!tnaCheckForThisCookie("dontShowCookieNotice")) {
+        $('<div class="cookieNotice">We use cookies to improve services and ensure they work for you. Read our <a href="http://www.nationalarchives.gov.uk/legal/cookies.htm">cookie policy</a>. <a href="#" id="cookieCutter">Close</a></div>').css({
+            padding: '5px',
+            "text-align" : "center",
+            backgroundColor : '#FCE45C',
+            position : 'fixed',
+            bottom : 0,
+            'font-size' : '14px',
+            width : '100%',
+            display : 'none'
+        }).appendTo('body');
+
+        setTimeout(function(){
+            $('.cookieNotice').slideDown(1000);
+        }, 1000);
+    }
+
+});
+
+// 2.4 Binding to document (event delegation)
+$(document).on('click', '#cookieCutter', function(e){
+    e.preventDefault();
+    tnaSetThisCookie('dontShowCookieNotice', 365);
+    $('.cookieNotice').hide();
+});
+// ----------------------------------------
