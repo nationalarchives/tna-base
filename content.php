67e75972a03b5b8baf7491a15887ef23213b9011
Chris Bishop <domingobishop@gmail.com>
2016-01-25 11:26:52 +0000

    if single condition added

--- content.php
+++ content.php
@@ -1,8 +1,19 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<h1><?php the_title(); ?></h1>
-	</div>
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div>
-</article>
+<?php if ( ! is_single() ) { ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="entry-header">
+			<h1><?php the_title(); ?></h1>
+		</div>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+	</article>
+<?php } else { ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<div class="entry-header">
+			<h1><?php the_title(); ?></h1>
+		</div>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+	</article>
+<?php } ?>
