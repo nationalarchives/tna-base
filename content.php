b88c5aa6273d246941b5bd74ae4bcacf8fd2e29b
Chris Bishop <domingobishop@gmail.com>
2016-01-26 12:49:48 +0000

    template sass created

--- content.php
+++ content.php
@@ -8,6 +8,7 @@
 		</div>
 	</article>
 <?php } else { ?>
+
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="entry-header">
 			<h1><?php the_title(); ?></h1>
@@ -16,4 +17,5 @@
 			<?php the_content(); ?>
 		</div>
 	</article>
+
 <?php } ?>
