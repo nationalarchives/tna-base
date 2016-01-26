1c236199e964747a1a83db2c23fd8e6745014fac
Chris Bishop <domingobishop@gmail.com>
2016-01-26 15:03:27 +0000

    dimox for breadcrumbs

--- breadcrumb.php
+++ breadcrumb.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<p>BREADCRUMB</p>
+				<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 			</div>
 		</div>
 	</div>
