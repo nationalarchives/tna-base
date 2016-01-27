4ec9548b25a21d06b06bacc60ae57d535595d01d
Punal Chotrani <punalsc@gmail.com>
2016-01-27 12:57:20 +0000

    header html

--- header.php
+++ header.php
@@ -8,6 +8,23 @@
 
 	<title><?php wp_title(''); ?></title>
 	<?php wp_head(); ?>
+
+	<!-- Fav Icon -->
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+	<link rel="icon" type="image/vnd.microsoft.icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-2827241-1']);
+		_gaq.push(['_setDomainName', 'nationalarchives.gov.uk']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
 </head>
 
 <body <?php body_class(); ?>>
