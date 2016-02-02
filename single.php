3cb4336317a2e67378b3b113b248fa50113ee0ea
Chris Bishop <domingobishop@gmail.com>
2016-02-02 15:17:02 +0000

    edited col class for sm

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row">
-				<main id="main" class="col-xs-12 col-sm-12 col-md-8" role="main">
+				<main id="main" class="col-xs-12 col-sm-8 col-md-8" role="main">
 					<?php
 					while ( have_posts() ) : the_post();
 						get_template_part( 'content' );
