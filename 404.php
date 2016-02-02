8953d77cca5f18bfa1c3efcab256a23f24cdec6b
Chris Bishop <domingobishop@gmail.com>
2016-02-02 10:56:59 +0000

    404 template id added

--- 404.php
+++ 404.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
+<?php get_template_part( 'breadcrumb' ); ?>
 
 	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row">
 				<main id="main" class="col-md-12" role="main">
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="entry-header text-center">
+					<article id="404-error">
+						<div class="entry-header">
 							<h1>Oops! That page can't be found</h1>
 						</div>
-						<div class="entry-content text-center">
+						<div class="entry-content">
 							<h2>404 Error</h2>
 							<p>It looks like nothing was found at this location. Maybe try a search?</p>
 							<?php get_search_form(); ?>
