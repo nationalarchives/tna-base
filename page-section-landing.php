367eaf56a809c2f09f70706a921dae8d654d3d45
Punal Chotrani <punalsc@gmail.com>
2016-02-02 16:44:40 +0000

    HTML Landing page block.

---
+++ page-section-landing.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Template Name: Section landing
+*/
+?>
+<?php get_header(); ?>
+<?php get_template_part ( 'breadcrumb' ); ?>
+
+    <div id="primary">
+        <div class="container">
+            <div class="row">
+                <main class="col-md-12">
+                    <!--Main loop begins HERE-->
+                        <article>
+                            <div class="entry-header">
+                                <h1>Main Title</h1>
+                            </div>
+                            <div class="col-xs-12 col-md-8 entry-full-width-content">
+                                <p>The very first appearance of Spider-Man. Once upon a time, Jessica Jones was a costumed super-hero, just not a very good one. But will each partner’s combined strength be enough. But her wit, charm and intelligence just may help her survive through another day.<p>
+
+                                <p>Now a chain-smoking, self-destructive alcoholic with a mean inferiority complex, Jones is the owner and sole employee of Alias Investigations - a small, private-investigative firm specializing in super-human cases. Learn the Wallcrawlers tragic origin and witness amazing fantasy as only Marvel can provide it. But her wit, charm and intelligence just may help her survive through another day. Amazing Spider-Man is the cornerstone of the Marvel Universe.</p>
+                            </div>
+                            <div class="col-xs-6 col-md-4 entry-full-width-content">
+                                <span>&nbsp;</span>
+                            </div>
+                        </article>
+                    <!--Main loop begins ENDS-->
+                </main>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <article>
+                        <div class="entry-header">
+                            <h2>Title 1</h2>
+                        </div>
+                        <div class="entry-content">
+                            <p>Avengers Assemble. Earths Mightiest Heroes reunite with their biggest guns at the forefront to take on familiar enemies and new threats alike. The Avengers return. New threats.</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-6">
+                    <article>
+                        <div class="entry-header">
+                            <h2>Title 2</h2>
+                        </div>
+                        <div class="entry-content">
+                            <p>Iron Man, Thor, Captain America and the rest of Earths Mightiest Heroes unite to stand against the threats none can face alone. Cables the son of original X-Man Scott Summers and the clone of Jean Grey who grew up in a war-torn future ruled by Apocalypse.</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
