d15bd234d3aa470a133239cd581f8ab4ebd7d650
Chris Bishop <domingobishop@gmail.com>
2016-02-02 09:50:24 +0000

    moved inc to bottom

--- content.php
+++ content.php
@@ -1,4 +1,5 @@
 <?php if ( ! is_single() ) { ?>
+	<!-- page.php -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="entry-header">
 			<h1><?php the_title(); ?></h1>
@@ -8,7 +9,7 @@
 		</div>
 	</article>
 <?php } else { ?>
-
+	<!-- singe.php -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="entry-header">
 			<h1><?php the_title(); ?></h1>
@@ -17,5 +18,4 @@
 			<?php the_content(); ?>
 		</div>
 	</article>
-
 <?php } ?>
