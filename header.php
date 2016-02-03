30da40893c42c03bb8b6f86f10e009e5dc6f822b
Chris Bishop <domingobishop@gmail.com>
2016-02-03 10:58:20 +0000

    relative paths for images

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
 	<title><?php wp_title(''); ?></title>
 	<?php wp_head(); ?>
 	<!-- Fav Icon -->
-	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
-	<link rel="icon" type="image/vnd.microsoft.icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/favicon.ico">
+	<link rel="icon" type="image/vnd.microsoft.icon" href="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/favicon.ico">
 	<script type="text/javascript">
 		var _gaq = _gaq || [];
 		_gaq.push(['_setAccount', 'UA-2827241-1']);
@@ -28,13 +28,13 @@
 			<div class="col-xs-3 col-sm-5" id="logo-holder">
 				<button aria-label="Toggle menu" id="mega-menu-mobile" ></button>
 				<a href="http://www.nationalarchives.gov.uk" title="Go to The National Archives homepage" class="visible-lg visible-md visible-sm">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.png" alt="The National Archives" id="logo">
+					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo">
 				</a>
 			</div>
 			<div class="col-xs-6 col-sm-2" id="mobile-logo-holder">
 				<button aria-label="Toggle menu" id="mega-menu-pull-down" class="hidden-xs"><span>Menu</span></button>
 				<a href="http://www.nationalarchives.gov.uk" title="Go to The National Archives homepage" class="hidden-lg hidden-md hidden-sm">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/logo-white.png" alt="The National Archives" id="logo">
+					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo">
 				</a>
 			</div>
 			<div class="col-xs-3 col-sm-5" id="search-field-wrapper">
--- sidebar.php
+++ sidebar.php
@@ -6,7 +6,7 @@ $parentid = $post->post_parent;
 <aside id="sidebar" class="col-xs-12 col-sm-4 col-md-4" role="complementary">
 	<div class="sidebar-header">
 		<h2>
-			<a name="inThisSection" href="<?php echo get_permalink($parentid); ?>">
+			<a name="inThisSection" href="<?php echo str_replace(home_url(), $pre_url, get_permalink($parentid)); ?>">
 				Also in <?php echo get_the_title($parentid);?>
 			</a>
 		</h2>
@@ -20,7 +20,7 @@ $parentid = $post->post_parent;
 			// manually on the WP edit page.
 			// We're using depth=1 to ensure we only get the children of the parent page, not grandchildren
 			// See http://codex.wordpress.org/Function_Reference/wp_list_pages for a full list of parameters
-			echo wp_list_pages("echo=0&title_li=&child_of=$parentid&sort_column=menu_order&depth=1&exclude=$post->ID");
+			echo str_replace(home_url(), $pre_url, wp_list_pages("echo=0&title_li=&child_of=$parentid&sort_column=menu_order&depth=1&exclude=$post->ID"));
 			?>
 		</ul>
 	</div>
