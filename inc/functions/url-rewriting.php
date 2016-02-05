1e6a46399b6cbb8adb7cecff12e16d098a3abe78
Chris Bishop <domingobishop@gmail.com>
2016-02-05 14:22:49 +0000

    comments added

--- inc/functions/url-rewriting.php
+++ inc/functions/url-rewriting.php
@@ -6,10 +6,12 @@ add_filter( 'style_loader_src', 'tna_styles_scripts_relative' );
 function tna_styles_scripts_relative( $url ) {
     return str_replace( site_url(), '', $url );
 }
+// Make template URLs relative
 function make_path_relative( $url ) {
     global $pre_path;
     return str_replace( site_url(), $pre_path, $url );
 }
+// Fix URLs in wp_head
 function tna_wp_head() {
     ob_start();
     wp_head();
