415d7180f273587c21c478b59795ed2bbda26e1a
Chris Bishop <domingobishop@gmail.com>
2016-02-09 18:20:44 +0000

    refactored header sass

--- header.php
+++ header.php
@@ -27,19 +27,19 @@
 <header id="header" role="banner"><a id="skip-to-main-content" href="#breadcrumb-holder">Skip to Main Content</a>
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-3 col-sm-5" id="logo-holder">
+			<div class="col-xs-2 col-sm-5" id="logo-holder">
 				<button aria-label="Toggle menu" id="mega-menu-mobile" ></button>
 				<a href="http://www.nationalarchives.gov.uk" title="Go to The National Archives homepage" class="visible-lg visible-md visible-sm">
-					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo">
+					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo" class="img-responsive">
 				</a>
 			</div>
-			<div class="col-xs-6 col-sm-2" id="mobile-logo-holder">
+			<div class="col-xs-8 col-sm-2" id="mobile-logo-holder">
 				<button aria-label="Toggle menu" id="mega-menu-pull-down" class="hidden-xs"><span>Menu</span></button>
 				<a href="http://www.nationalarchives.gov.uk" title="Go to The National Archives homepage" class="hidden-lg hidden-md hidden-sm">
-					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo">
+					<img src="<?php echo str_replace(home_url(), '', get_template_directory_uri()); ?>/img/logo-white.png" alt="The National Archives" id="logo" class="img-responsive">
 				</a>
 			</div>
-			<div class="col-xs-3 col-sm-5" id="search-field-wrapper">
+			<div class="col-xs-2 col-sm-5" id="search-field-wrapper">
 				<form action="http://www.nationalarchives.gov.uk/search/quick_search.aspx" method="get" id="globalSearch" class="hidden-xs">
 					<span id="scope-selector">&nbsp;</span>
 					<input type="text" class="search-field" placeholder="Search our website..." id="tnaSearch" name="search_text" required aria-required="true" >
