65a28e73a9034765ecdd7b09dde87feef947327c
Chris Bishop <domingobishop@gmail.com>
2016-02-09 16:02:03 +0000

    level one page links fix

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,9 @@
 <?php
 // This gets home and parent page IDs
-$home_id = get_option('page_on_front');
 $parent_id = $post->post_parent;
-
+$home_id = get_option('page_on_front');
 // This gets the link to the parent page, based on the parent page ID
-$parent_page_id = ($parent_id == $home_id ? $home_id : $parent_id);
+$parent_page_id = ($parent_id == 0 ? get_option('page_on_front') : $parent_id);
 ?>
 
 <aside id="sidebar" class="col-xs-12 col-sm-4 col-md-4" role="complementary">
@@ -24,7 +23,7 @@ $parent_page_id = ($parent_id == $home_id ? $home_id : $parent_id);
 			// manually on the WP edit page.
 			// We're using depth=1 to ensure we only get the children of the parent page, not grandchildren
 			// See http://codex.wordpress.org/Function_Reference/wp_list_pages for a full list of parameters
-			echo make_path_relative( wp_list_pages("echo=0&title_li=&child_of=$parent_id&sort_column=menu_order&depth=1&exclude=$post->ID,$parent_page_id") );
+			echo make_path_relative( wp_list_pages("echo=0&title_li=&child_of=$parent_id&sort_column=menu_order&depth=1&exclude=$post->ID,$home_id") );
 			?>
 		</ul>
 	</div>
