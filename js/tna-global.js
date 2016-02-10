b39382835e13e4dc42ba3b76990fe2bd636c3a66
Chris Bishop <domingobishop@gmail.com>
2016-02-10 17:15:17 +0000

    equal heights quick fix

--- js/tna-global.js
+++ js/tna-global.js
@@ -265,11 +265,11 @@ $(document).ready(function(){
 $('#equal-heights').fadeIn('slow');
 
 $(window).load(function() {
-    equalheight('#equal-heights > div > article');
+    equalheight('#equal-heights > div');
 });
 
 $(window).resize(function(){
-    equalheight('#equal-heights > div > article');
+    equalheight('#equal-heights > div');
 });
 
 
