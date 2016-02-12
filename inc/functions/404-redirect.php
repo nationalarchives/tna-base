9a6bab197d3a8d7cce18666aec40ce62a8dc7ee8
Chris Bishop <domingobishop@gmail.com>
2016-02-12 11:31:44 +0000

    404 fix for test

--- inc/functions/404-redirect.php
+++ inc/functions/404-redirect.php
@@ -1,22 +1,21 @@
 <?php
 // 404 Redirection function - called in header.php
-if (!function_exists('redirect_if_404')) :
-	function redirect_if_404()
-	{
-		if (is_404()) {
+if ( ! function_exists( 'redirect_if_404' ) ) :
+	function redirect_if_404() {
+		if ( is_404() ) {
+			// Pre path for child sites
+			global $pre_path;
 			// Format string with placeholders for components
 			$requested_page = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 			// Sanitizing the URL before use
-			$requested_page_safe = filter_var($requested_page, FILTER_SANITIZE_URL);
-			// Staging URLs
-			$staging_subdomains = array("devlb", "testlb", "livelb");
-			$requested_page_safe = str_replace($staging_subdomains, "www", $requested_page_safe);
+			$requested_page_safe = filter_var( $requested_page, FILTER_SANITIZE_URL );
+			$requested_page_safe = str_replace( site_url(), 'http://www.nationalarchives.gov.uk' . $pre_path, $requested_page_safe );
 			// Format string for redirection app URL with single placeholder
 			$redirect_app_url_format = "http://www.nationalarchives.gov.uk/PageNotFound/PageNotFound.aspx?url=%s";
-			$redirection_url = sprintf($redirect_app_url_format, $requested_page_safe);
-			wp_redirect($redirection_url, 301);
+			$redirection_url         = sprintf( $redirect_app_url_format, $requested_page_safe );
+			wp_redirect( $redirection_url, 301 );
 			exit;
 		}
 	}
 endif;
-add_filter('template_redirect', 'redirect_if_404');
+add_filter( 'template_redirect', 'redirect_if_404' );
