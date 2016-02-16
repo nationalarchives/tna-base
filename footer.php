fcb9cb398950f4e77ff866f4f7f33b22008bf8a3
Chris Bishop <domingobishop@gmail.com>
2016-02-16 13:00:46 +0000

    added webtrends

--- footer.php
+++ footer.php
@@ -7,5 +7,26 @@
 
 <?php wp_footer(); ?>
 
+<!-- START OF WebTrends -->
+<script type="text/javascript">
+	//<![CDATA[
+	var _tag=new WebTrends();
+	_tag.dcsGetId();
+	//]]>>
+</script>
+<script type="text/javascript">
+	//<![CDATA[
+	// Add custom parameters here.
+	//_tag.DCSext.param_name=param_value;
+	_tag.dcsCollect();
+	//]]>>
+</script>
+<noscript>
+	<div>
+		<img id="DCSIMG" height="1" alt="DCSIMG" src="http://smartsource.nationalarchives.gov.uk/dcsdhhxq6000004rry7ab39or_9h9r/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2" width=1 />
+	</div>
+</noscript>
+<!-- END OF WebTrends -->
+
 </body>
 </html>
