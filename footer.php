272a83d521fc0b148fff75973484551cfe323486
Chris Bishop <domingobishop@gmail.com>
2016-02-16 13:05:08 +0000

    footer inc

--- footer.php
+++ footer.php
@@ -6,27 +6,7 @@
 </footer>
 
 <?php wp_footer(); ?>
-
-<!-- START OF WebTrends -->
-<script type="text/javascript">
-	//<![CDATA[
-	var _tag=new WebTrends();
-	_tag.dcsGetId();
-	//]]>>
-</script>
-<script type="text/javascript">
-	//<![CDATA[
-	// Add custom parameters here.
-	//_tag.DCSext.param_name=param_value;
-	_tag.dcsCollect();
-	//]]>>
-</script>
-<noscript>
-	<div>
-		<img id="DCSIMG" height="1" alt="DCSIMG" src="http://smartsource.nationalarchives.gov.uk/dcsdhhxq6000004rry7ab39or_9h9r/njs.gif?dcsuri=/nojavascript&amp;WT.js=No&amp;WT.tv=8.6.2" width=1 />
-	</div>
-</noscript>
-<!-- END OF WebTrends -->
+<?php get_template_part( 'inc/content/tna', 'footer' ); ?>
 
 </body>
 </html>
