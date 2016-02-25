b7be6a9838ef208f3836d63e5b6b79973e926621
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-02-25 11:49:55 +0000

    Title tags added to the parent theme on some features like e.g. go back to top button, search options etc.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<a href="#" id="goTop"></a>
+<a title="Go back to top" href="#" id="goTop"></a>
 <footer id="footer" class="breather-top-bottom" role="contentinfo">
 	<div class="container">
 		<?php get_template_part( 'inc/content/footer-content' ); ?>
