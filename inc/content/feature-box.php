2c393d549210e1db8487ad1891745e8c94ecfc56
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-03-01 16:40:08 +0000

    Added image caption functionality to the parent theme

--- inc/content/feature-box.php
+++ inc/content/feature-box.php
@@ -2,7 +2,14 @@
 global $post;
 $content_with_feat_box = '<div class="col-xs-12 col-sm-8 col-md-8">';
 $content_with_feat_img = '<div class="col-xs-12 col-sm-6 col-md-6">';
+
 $feat_box = get_post_meta(get_the_ID(), 'feat_box', true);
+$img_caption_desc = get_post_meta(get_post_thumbnail_id(), 'image-caption-description', true);
+$img_caption_url = get_post_meta(get_post_thumbnail_id(), 'image-caption-url', true);
+$img_caption_url_desc = get_post_meta(get_post_thumbnail_id(), 'image-caption-url-desc', true);
+
+
+
 if (!empty( $feat_box )) { // This is the custom field block
 	echo $content_with_feat_box;
 	if (have_posts()) :
@@ -21,10 +28,24 @@ if (!empty( $feat_box )) { // This is the custom field block
 			the_content();
 			echo '</div>';
 			echo '<div class="col-xs-12 col-sm-6 col-md-6">';
-								$image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'landing-page-children-thumb' );
-	                            ?>
-	                            <img src="<?php echo(make_path_relative($image_url[0])); ?>" class="img-responsive" alt="<?php echo $post->post_title ?>">
-	                            <?php
+				$image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'landing-page-children-thumb' );
+				?>
+				<img src="<?php echo make_path_relative($image_url[0]); ?>" class="img-responsive" alt="<?php echo $post->post_title ?>">
+				<?php if(!empty($img_caption_desc) && !empty($img_caption_url)) :?>
+						<div role="button" class="eye_caption">&nbsp;</div>
+						<div class="image_caption_back">
+							<?php echo $img_caption_desc; ?>
+							<div class="clearfix"></div>
+							<a href="<?php echo $img_caption_url ?>" target="_blank">
+								<?php if  (empty($img_caption_url_desc) ) :?>
+									View in the image library
+								<?php else: ?>
+									<?php echo $img_caption_url_desc ; ?>
+								<?php endif; ?>
+							</a>
+						</div>
+				<?php endif; ?>
+				<?php
 			echo '</div>';
 		endwhile;
 	endif;
---
+++ inc/functions/image_caption.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Add Image caption description and URL fields to media uploader
+ *
+ * @param $form_fields array, fields to include in attachment form
+ * @param $post object, attachment record in database
+ * @return $form_fields, modified form fields
+ */
+
+function image_caption_fields( $form_fields, $post ) {
+    $form_fields['image-caption-description'] = array(
+        'label' => 'Image description',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'image-caption-description', true ),
+        'helps' => 'Add image caption description',
+    );
+
+    $form_fields['image-caption-url'] = array(
+        'label' => 'Image caption URL',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'image-caption-url', true ),
+        'helps' => 'Add image caption URL',
+    );
+
+    $form_fields['image-caption-url-desc'] = array(
+        'label' => 'Image link text',
+        'input' => 'text',
+        'value' => get_post_meta( $post->ID, 'image-caption-url-desc', true ),
+        'helps' => 'Optional, add link text. If left blank default text is "View in the image library"',
+    );
+
+    return $form_fields;
+}
+
+add_filter( 'attachment_fields_to_edit', 'image_caption_fields', 10, 2 );
+
+/**
+ * Save values of Image caption description and URL in media uploader
+ *
+ * @param $post array, the post data for database
+ * @param $attachment array, attachment fields from $_POST form
+ * @return $post array, modified post data
+ */
+
+function save_image_caption_fields( $post, $attachment ) {
+    if( isset( $attachment['image-caption-description'] ) )
+        update_post_meta( $post['ID'], 'image-caption-description', $attachment['image-caption-description'] );
+
+    if( isset( $attachment['image-caption-url'] ) )
+        update_post_meta( $post['ID'], 'image-caption-url', esc_url( $attachment['image-caption-url'] ) );
+
+    if( isset( $attachment['image-caption-url-desc'] ) )
+        update_post_meta( $post['ID'], 'image-caption-url-desc',  $attachment['image-caption-url-desc'] ) ;
+
+    return $post;
+}
+
+add_filter( 'attachment_fields_to_save', 'save_image_caption_fields', 10, 2 );
+
+?>
