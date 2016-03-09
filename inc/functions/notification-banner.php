08f643cf1666a33d6e84f695d7a2cef7bf5d2f1b
Chris Bishop <domingobishop@gmail.com>
2016-03-09 13:52:40 +0000

    added variables

--- inc/functions/notification-banner.php
+++ inc/functions/notification-banner.php
@@ -1,15 +1,19 @@
 <?php
-
+// Notification banner
 function notification_banner() {
+	$notice_title = 'YO MOTHER';
+	$notice_text = 'This is a new service';
+	$notice_link = '#';
+	$notice_link_text = 'send us feedback';
 	?>
 	<div class="notification-banner">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<div class="notice">
-						<strong>BETA</strong>
-						This is a new service -
-						<a href="#">send us feedback</a>
+						<strong><?php echo $notice_title; ?></strong>
+						<?php echo $notice_text; ?> -
+						<a href="<?php echo $notice_link; ?>"><?php echo $notice_link_text; ?></a>
 					</div>
 				</div>
 			</div>
