21b14a784417ee28b0e4256d596df94f65517de6
Chris Bishop <domingobishop@gmail.com>
2016-06-13 15:33:39 +0100

    level 1 banner css

--- page-level-1-landing.php
+++ page-level-1-landing.php
@@ -5,23 +5,26 @@ Template Name: Level 1 landing
 get_header(); ?>
 <?php get_template_part( 'breadcrumb' ); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="level-one">
+		<?php while ( have_posts() ) : the_post(); ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<article class="banner">
-						<div class="entry-header"
-							<?php
-							if ( has_post_thumbnail() ) {
-								$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-								?>
-								style="background: url(<?php echo make_path_relative( $thumbnail_src[0] ); ?>) center center;background-size: cover;height: 288px;"
-							<?php } ?>>
+					<article class="banner" <?php
+					if ( has_post_thumbnail() ) {
+						$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+						style="background: url(<?php echo make_path_relative( $thumbnail_src[0] ); ?>) center center;background-size: cover;"
+					<?php } ?>>
+						<div class="entry-header">
 							<h1><?php the_title(); ?></h1>
-							<div class="entry-header-content">
-								<?php the_content(); ?>
-							</div>
 						</div>
+							<?php if( empty( $post->post_content) ) {
+								// Do nothing
+							} else { ?>
+								<div class="entry-content">
+								<?php the_content(); ?>
+								</div>
+							<?php } ?>
 					</article>
 				</div>
 			</div>
@@ -80,6 +83,7 @@ get_header(); ?>
 				</main>
 			</div>
 		</div>
+		<?php endwhile; ?>
 	</div>
 
 <?php get_footer(); ?>
