b0632b201bcd603dc0d178d9639c1579a2b7bb76
Chris Bishop <domingobishop@gmail.com>
2016-07-08 11:36:53 +0100

    index post loop

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
 			<main id="main" class="col-sm-12" role="main">
 				<?php
 				if ( have_posts() ) :
-					while ( have_posts() ) : the_post();
-						get_template_part( 'content' );
-					endwhile;
+					while ( have_posts() ) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="entry-header">
+								<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+							</div>
+							<div class="entry-content clearfix">
+								<?php the_excerpt(); ?>
+								<a href="<?php the_permalink(); ?>" class="button pull-right" title="Read more - <?php the_title(); ?>">Read more</a>
+							</div>
+						</article>
+					<?php endwhile;
 				else : ?>
 					<article>
 						<div class="entry-header">
@@ -20,12 +28,12 @@
 						</div>
 					</article>
 				<?php endif; ?>
-				<div>
+				<nav>
 					<ul class="pager">
-						<li class="previous"><?php next_posts_link(__('&#8249; Older posts' )); ?></li>
-						<li class="next"><?php previous_posts_link(__('Newer posts &#8250;')); ?></li>
+						<li class="previous"><?php next_posts_link( '&laquo; Older posts' ); ?></li>
+						<li class="next"><?php previous_posts_link( 'Newer posts &raquo;' ); ?></li>
 					</ul>
-				</div>
+				</nav>
 			</main>
 		</div>
 	</div>
