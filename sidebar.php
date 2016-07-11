9e74a2e77c69c9d83c71ede5bcf8180f19e2f4ce
Chris Bishop <domingobishop@gmail.com>
2016-07-11 16:04:56 +0100

    replaced wp_list_pages with get_pages to include the redirect url meta data

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,18 @@
-<?php
-// This gets home and parent page IDs
-$parent_id = $post->post_parent;
-$home_id = get_option('page_on_front');
-// This gets the link to the parent page, based on the parent page ID
-$parent_page_id = ($parent_id == 0 ? get_option('page_on_front') : $parent_id);
-?>
-
-<aside id="sidebar" class="col-xs-12 col-sm-4 col-md-4" role="complementary">
+<aside id="sidebar" class="col-xs-12 col-sm-4" role="complementary">
 	<div class="sidebar-header">
 		<h2>
-			<a name="inThisSection" href="<?php echo make_path_relative( get_permalink($parent_page_id) ); ?>">
+			<?php
+			// This gets home and parent page IDs
+			$parent_id = $post->post_parent;
+			$home_id = get_option('page_on_front');
+			// This gets the link to the parent page, based on the parent page ID
+			$parent_page_id = ($parent_id == 0 ? get_option('page_on_front') : $parent_id);
+			$redirectHeading = get_post_meta( $parent_page_id, 'redirectUrl', true );
+			if ( $redirectHeading ) { ?>
+				<a href="<?php echo $redirectHeading; ?>">
+			<?php } else { ?>
+				<a href="<?php echo make_path_relative( get_permalink($parent_page_id) ); ?>">
+			<?php } ?>
 				Also in <?php echo get_the_title($parent_page_id);?>
 			</a>
 		</h2>
@@ -17,14 +20,32 @@ $parent_page_id = ($parent_id == 0 ? get_option('page_on_front') : $parent_id);
 	<div class="sidebar-nav clearfix">
 		<ul class="sibling">
 			<?php
-			// This uses wp_list_pages to get the list of siblings of the current page. However we're actually
-			// showing the children of the parent page. We're also excluding the current page ID ($post->ID)
-			// so as not to duplicate it in the navigation and sorting the links by their menu order, as set
-			// manually on the WP edit page.
-			// We're using depth=1 to ensure we only get the children of the parent page, not grandchildren
-			// See http://codex.wordpress.org/Function_Reference/wp_list_pages for a full list of parameters
-			echo make_path_relative( wp_list_pages("echo=0&title_li=&child_of=$parent_id&sort_column=menu_order&depth=1&exclude=$post->ID,$home_id") );
-			?>
+			$args = array(
+				'child_of' => $parent_id,
+				'parent' => $parent_id,
+				'hierarchical' => 0,
+				'sort_column' => 'menu_order',
+				'exclude' => array( $post->ID, $home_id ),
+				'post_type' => 'page',
+				'post_status' => 'publish'
+			);
+			$pages = get_pages( $args );
+			foreach ( $pages as $page ) {
+			    $redirect = get_post_meta( $page->ID, 'redirectUrl', true );
+				if ( $redirect ) { ?>
+					<li>
+						<a href="<?php echo $redirect; ?>" title="<?php echo $page->post_title ?>">
+							<?php echo $page->post_title; ?>
+						</a>
+					</li>
+				<?php } else { ?>
+					<li>
+						<a href="<?php echo make_path_relative( get_page_link( $page->ID ) ); ?>" title="<?php echo $page->post_title ?>">
+							<?php echo $page->post_title; ?>
+						</a>
+					</li>
+				<?php }
+			} ?>
 		</ul>
 	</div>
 </aside>
