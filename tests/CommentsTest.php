6d68ef1240a07b2cc3cfa4b3fa6f8d26fa5f7311
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-08-17 14:35:40 +0100

    Fixed some alignment issues

--- tests/CommentsTest.php
+++ tests/CommentsTest.php
@@ -4,6 +4,8 @@ class CommentsTest extends PHPUnit_Framework_TestCase
 {
     /**
      *  Test if comments.php template exists
+     * commnets.php is mandatory since wordpress version 3.0
+     * If deleted a php error will be displayed in debug mode
      */
     public function testIfCommExists()
     {
