408045e4445f479fa815b97908f691f18097867c
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-08-24 16:33:05 +0100

    Added new changes to category template

--- category.php
+++ category.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <main id="main" class="col-sm-12" role="main">
-                <article>
+                <article class="equal-heights">
                     <div class="entry-header">
                         <h1><?php foreach ((get_the_category()) as $category) {
                                 echo $category->cat_name . ' ';
@@ -13,41 +13,42 @@
                     </div>
 
                     <?php
-                        if (have_posts()) :
-                        while (have_posts()) : the_post(); ?>
+                    if (have_posts()) :
+                    while (have_posts()) :
+                    the_post(); ?>
 
                     <?php
-                        $page_id = $post->ID;
-                        $image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
-                        $childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
+                    $page_id = $post->ID;
+                    $image = wp_get_attachment_image_src(get_post_thumbnail_id($page_id), 'single-post-thumbnail');
+                    $childimage = wp_get_attachment_image_src(get_post_thumbnail_id($post->post_parent), 'single-post-thumbnail');
                     ?>
                     <div class="col-md-4">
                         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>">
-                            <div class="image_back" style="background-image: url('<?php
+                        <div class="image_back" style="background-image: url('<?php
 
-                                if (has_post_thumbnail($page_id)) {
-                                    echo make_path_relative($image[0]);
-                                } else {
-                                    echo bloginfo('template_directory') . '/img/gov-uk.png';
-                                }
-                                ?>')">
+                        if (has_post_thumbnail($page_id)) {
+                            echo make_path_relative($image[0]);
+                        } else {
+                            echo bloginfo('template_directory') . '/img/no-image.pg';
+                        }
+                        ?>')">
 
-                            </div>
-                            <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-                            </h2>
+                        </div>
+                        <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                        </h2>
 
-                            <div class="entry-content clearfix">
-                                <?php the_excerpt(); ?>
-                                <a href="<?php the_permalink(); ?>" class="button pull-right"
-                                   title="Read more - <?php the_title(); ?>">Read more</a>
-                            </div>
-                         </article>
+                        <div class="entry-content clearfix">
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>" class="button pull-right"
+                               title="Read more - <?php the_title(); ?>">Read more</a>
+                        </div>
+                </article>
         </div>
         <?php endwhile;
         else : ?>
             <article>
                 <div class="entry-header">
-                    <h1>Nothing found</h1>
+                    <h1>Nothing found</h1>glossary
                 </div>
                 <div class="entry-content clearfix">
                     <p>It appears we don't have any published posts.</p>
