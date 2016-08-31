ed773bc87e868a7fe3afb3afe112b3d7199de00c
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-08-31 21:02:39 +0100

    Introduce JavaScript that manages mega menu
    This includes a number of jQuery plugins that have been written with associated tests.

--- js/run-on-page-load.js
+++ js/run-on-page-load.js
@@ -1,1 +1,9 @@
 $('a[target="_blank"]').add_attributes_to_target_blank();
+
+$('#mega-menu-pull-down, #mega-menu-mobile').mega_menu_interactions();
+
+$("ul.sub-menu:last").append_promotional_image();
+
+$(".main-ul > li > a").append_home_links_to_mega_menu();
+
+$('a', '.mega-menu').webtrends_click_handler();
