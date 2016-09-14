32d4417dbec75427bb146b84beea59b80dbe5859
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-09-14 13:58:31 +0100

    Refactoring from Pull Request

--- js/tests/newsletter_validation/tests.js
+++ js/tests/newsletter_validation/tests.js
@@ -10,20 +10,26 @@ QUnit.test("Check required elements are available in fixture", function (assert)
     assert.equal($('input[type=email]', '.fixture').val(), "", "The email address is empty");
 });
 
-QUnit.module("Checking the utilities", {
+QUnit.module("Checking the custom utilities created for this plugin", {
     beforeEach: function () {
         $('input[type=email]', '.fixture').val('');
     }
 });
+
 QUnit.test("Check if the browser supports HTML5 validation", function (assert) {
     assert.equal($.html5ValidationAvailable(), true);
 });
 
-QUnit.test("Test $.isEmail() function", function (assert) {
-    assert.equal($.isEmail('john'), false, "John is not an email address");
-    assert.equal($.isEmail('john@com'), false, "john@com is not an email address");
-    assert.equal($.isEmail('john.com'), false, "john.com is not an email address");
-    assert.equal($.isEmail('john@john.com'), true, "john@john.com is an email address");
+QUnit.test("Test $.isEmail() function 'happy path'", function (assert) {
+
+    ['john@john.com', 'mary@mary.com'].forEach(function (i) {
+        assert.equal($.isEmail(i), true, "john@john.com is an email address");
+    });
+
+    ['john', 'mary', 'john.com', 'john@com'].forEach(function (i) {
+        assert.equal($.isEmail(i), false, "John is not an email address");
+    });
+
 });
 
 QUnit.module("Checking the interactions", {
@@ -36,23 +42,10 @@ QUnit.module("Checking the interactions", {
     }
 });
 
-
 QUnit.test("The error message is created by the plugin", function (assert) {
     assert.equal($('.error', '.fixture').length, 0, "There is no .error div before the plugin has run");
     $('form', '.fixture').newsletterValidation();
     assert.equal($('.error', '.fixture').length, 1, "The .error div is present after the plugin has run");
-});
-
-QUnit.test("There is no error message shown initially", function (assert) {
-    $('form', '.fixture').newsletterValidation();
     var display = $('.error', '.fixture').css('display');
     assert.equal(display, 'none', "The .error div has 'display: none'");
-
-});
-
-QUnit.test("The error message is shown when incorrect content is entered", function (assert) {
-    $('form', '.fixture').newsletterValidation();
-    $('input[type=email]', '.fixture').val('wrong input').trigger('change');
-    var display = $('.error', '.fixture').css('display');
-    assert.equal(display, 'block', "The .error div has 'display: none'");
 });
