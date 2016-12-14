41593aaaf78618deb5046a5cd0ffafdb45b61058
Chris Bishop <domingobishop@gmail.com>
2016-12-14 11:12:05 +0000

    added sidebar file email and phone icons

---
+++ sidebar-contact.php
@@ -0,0 +1,43 @@
+<aside id="sidebar" class="col-xs-12 col-sm-4" role="complementary">
+	<div class="sidebar-header">
+		<h2>
+			<a href="/contact-us/">
+					Contact us for advice
+			</a>
+		</h2>
+	</div>
+	<div class="sidebar-nav clearfix">
+			<h3>Still need help?</h3>
+			<div class="live-chat">
+
+			</div>
+			<!-- end .breather -->
+			<div class="email-us">
+				<div class="width-full-to-three-quarters float-left">
+					<h3 class="margin-none margin-bottom-small"><a href="/contact/contactform.asp?id=22" title="Email us">Email</a></h3>
+					<p class="margin-none pad-none">For more detailed research enquiries.</p>
+				</div>
+				<!-- end .width-full-to-three-quarters -->
+				<div class="width-full-to-one-quarter float-left">
+					<a href="/contact/contactform.asp?id=22">
+						<img src="/wp-content/themes/tna/images/global/icon-128-email.png" alt="Email us">
+					</a>
+				</div>
+				<!-- end .width-full-to-one-quarter -->
+			</div>
+			<!-- end .breather -->
+			<div class="breather clr">
+				<div class="width-full-to-three-quarters float-left">
+					<h3 class="margin-none margin-bottom-small"><a href="/contact/#phone" title="Phone us">Phone</a></h3>
+					<p class="margin-none pad-none">+44 (0) 20 8876 3444</p>
+				</div>
+				<!-- end .width-full-to-three-quarters -->
+				<div class="width-full-to-one-quarter float-left">
+					<a href="/contact/#phone">
+						<img src="/wp-content/themes/tna/images/global/icon-128-phone.png" alt="Phone us">
+					</a>
+				</div>
+				<!-- end .width-full-to-one-quarter -->
+			</div>
+	</div>
+</aside>
