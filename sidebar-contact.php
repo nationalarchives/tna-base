11aa46818a8334788b9acb5aaa8a53f18ca6399e
Chris Bishop <domingobishop@gmail.com>
2016-12-14 11:37:36 +0000

    contact content

--- sidebar-contact.php
+++ sidebar-contact.php
@@ -6,38 +6,48 @@
 			</a>
 		</h2>
 	</div>
-	<div class="sidebar-nav clearfix">
-			<h3>Still need help?</h3>
-			<div class="live-chat">
-
+	<div class="sidebar-contact clearfix">
+			<div class="live-chat sidebar-item">
+				<div class="col-md-8">
+					<h3><a href="/contact/#livechat">Live chat</a></h3>
+					<p>For quick pointers<br>Tuesday to Saturday<br>
+						09:00 to 17:00
+					</p>
+				</div>
+				<div class="col-md-4">
+					<!-- BEGIN ProvideSupport.com Graphics Chat Button Code -->
+					<div id="ciEvtM"></div><div id="scEvtM"></div><div id="sdEvtM"></div>
+					<script type="text/javascript">
+						var seEvtM=document.createElement("script");seEvtM.type="text/javascript";var seEvtMs=(location.protocol.indexOf("https")==0?"https":"http")+"://image.providesupport.com/js/0or0ihkh4hylp1qnz87w7c01gr/safe-standard.js?ps_h=EvtM&ps_t="+new Date().getTime();setTimeout("seEvtM.src=seEvtMs;document.getElementById('sdEvtM').appendChild(seEvtM)",1)
+					</script>
+					<noscript>
+						<a href="http://www.providesupport.com?messenger=0or0ihkh4hylp1qnz87w7c01gr">Live Help Desk</a>
+					</noscript>
+					<!-- END ProvideSupport.com Graphics Chat Button Code -->
+				</div>
 			</div>
 			<!-- end .breather -->
-			<div class="email-us">
-				<div class="width-full-to-three-quarters float-left">
-					<h3 class="margin-none margin-bottom-small"><a href="/contact/contactform.asp?id=22" title="Email us">Email</a></h3>
-					<p class="margin-none pad-none">For more detailed research enquiries.</p>
+			<div class="email-us sidebar-item">
+				<div class="col-md-8">
+					<h3><a href="/contact-us/" title="Email us">Email</a></h3>
+					<p class="margin-none pad-none">For more detailed enquiries.</p>
 				</div>
-				<!-- end .width-full-to-three-quarters -->
-				<div class="width-full-to-one-quarter float-left">
-					<a href="/contact/contactform.asp?id=22">
-						<img src="/wp-content/themes/tna/images/global/icon-128-email.png" alt="Email us">
+				<div class="col-md-4">
+					<a href="/contact-us/">
+						<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-email.png" alt="Email us" class="img-responsive">
 					</a>
 				</div>
-				<!-- end .width-full-to-one-quarter -->
 			</div>
-			<!-- end .breather -->
-			<div class="breather clr">
-				<div class="width-full-to-three-quarters float-left">
-					<h3 class="margin-none margin-bottom-small"><a href="/contact/#phone" title="Phone us">Phone</a></h3>
+			<div class="phone sidebar-item">
+				<div class="col-md-8">
+					<h3><a href="/contact-us/" title="Phone us">Phone</a></h3>
 					<p class="margin-none pad-none">+44 (0) 20 8876 3444</p>
 				</div>
-				<!-- end .width-full-to-three-quarters -->
-				<div class="width-full-to-one-quarter float-left">
-					<a href="/contact/#phone">
-						<img src="/wp-content/themes/tna/images/global/icon-128-phone.png" alt="Phone us">
+				<div class="col-md-4">
+					<a href="/contact-us/">
+						<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-phone.png" alt="Phone us" class="img-responsive">
 					</a>
 				</div>
-				<!-- end .width-full-to-one-quarter -->
 			</div>
 	</div>
 </aside>
