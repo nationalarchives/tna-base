f1d41d0f9fca50083dd43e53eda8cfe93c3c77e6
Chris Bishop <domingobishop@gmail.com>
2016-12-14 16:16:12 +0000

    new post icon

--- sidebar-contact.php
+++ sidebar-contact.php
@@ -1,53 +1,56 @@
 <aside id="sidebar" class="col-xs-12 col-sm-4" role="complementary">
 	<div class="sidebar-header">
 		<h2>
-			<a href="/contact-us/">
-					Contact us for advice
-			</a>
+				Other ways to contact us
 		</h2>
 	</div>
 	<div class="sidebar-contact clearfix">
-			<div class="live-chat sidebar-item">
-				<div class="col-md-8">
-					<h3><a href="/contact/#livechat">Live chat</a></h3>
-					<p>For quick pointers<br>Tuesday to Saturday<br>
-						09:00 to 17:00
-					</p>
-				</div>
-				<div class="col-md-4">
-					<!-- BEGIN ProvideSupport.com Graphics Chat Button Code -->
-					<div id="ciEvtM"></div><div id="scEvtM"></div><div id="sdEvtM"></div>
-					<script type="text/javascript">
-						var seEvtM=document.createElement("script");seEvtM.type="text/javascript";var seEvtMs=(location.protocol.indexOf("https")==0?"https":"http")+"://image.providesupport.com/js/0or0ihkh4hylp1qnz87w7c01gr/safe-standard.js?ps_h=EvtM&ps_t="+new Date().getTime();setTimeout("seEvtM.src=seEvtMs;document.getElementById('sdEvtM').appendChild(seEvtM)",1)
-					</script>
-					<noscript>
-						<a href="http://www.providesupport.com?messenger=0or0ihkh4hylp1qnz87w7c01gr">Live Help Desk</a>
-					</noscript>
-					<!-- END ProvideSupport.com Graphics Chat Button Code -->
-				</div>
+		<div class="live-chat sidebar-item">
+			<div class="col-md-8">
+				<h3>Live chat</h3>
+				<p>
+					For quick pointers<br>
+					Tuesday to Saturday<br>
+					09:00 to 17:00
+				</p>
 			</div>
-			<!-- end .breather -->
-			<div class="email-us sidebar-item">
-				<div class="col-md-8">
-					<h3><a href="/contact-us/" title="Email us">Email</a></h3>
-					<p class="margin-none pad-none">For more detailed enquiries.</p>
-				</div>
-				<div class="col-md-4">
-					<a href="/contact-us/">
-						<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-email.png" alt="Email us" class="img-responsive">
-					</a>
-				</div>
+			<div class="col-md-4">
+				<!-- BEGIN ProvideSupport.com Graphics Chat Button Code -->
+				<div id="ciEvtM"></div><div id="scEvtM"></div><div id="sdEvtM"></div>
+				<script type="text/javascript">
+					var seEvtM=document.createElement("script");seEvtM.type="text/javascript";var seEvtMs=(location.protocol.indexOf("https")==0?"https":"http")+"://image.providesupport.com/js/0or0ihkh4hylp1qnz87w7c01gr/safe-standard.js?ps_h=EvtM&ps_t="+new Date().getTime();setTimeout("seEvtM.src=seEvtMs;document.getElementById('sdEvtM').appendChild(seEvtM)",1)
+				</script>
+				<noscript>
+					<a href="http://www.providesupport.com?messenger=0or0ihkh4hylp1qnz87w7c01gr">Live Help Desk</a>
+				</noscript>
+				<!-- END ProvideSupport.com Graphics Chat Button Code -->
 			</div>
-			<div class="phone sidebar-item">
-				<div class="col-md-8">
-					<h3><a href="/contact-us/" title="Phone us">Phone</a></h3>
-					<p class="margin-none pad-none">+44 (0) 20 8876 3444</p>
-				</div>
-				<div class="col-md-4">
-					<a href="/contact-us/">
-						<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-phone.png" alt="Phone us" class="img-responsive">
-					</a>
-				</div>
+		</div>
+		<div class="phone sidebar-item">
+			<div class="col-md-8">
+				<h3>Phone</h3>
+				<p>+44 (0) 20 8876 3444</p>
 			</div>
+			<div class="col-md-4">
+				<p>
+					<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-phone.png" alt="Phone us" class="img-responsive">
+				</p>
+			</div>
+		</div>
+		<div class="postal sidebar-item">
+			<div class="col-md-8">
+				<h3>Post</h3>
+				<p>
+					Please write to: <br>
+					The National Archives<br>
+					Kew, Richmond, Surrey, TW9 4DU
+				</p>
+			</div>
+			<div class="col-md-4">
+				<p>
+					<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-post.png" alt="Email us" class="img-responsive">
+				</p>
+			</div>
+		</div>
 	</div>
 </aside>
