507741d8ad426b2b758765284c8cf1cf5361b94c
Chris Bishop <domingobishop@gmail.com>
2016-12-16 15:37:37 +0000

    new mobile icon amended responsive col

--- sidebar-contact.php
+++ sidebar-contact.php
@@ -6,47 +6,45 @@
 	</div>
 	<div class="sidebar-contact clearfix">
 		<div class="live-chat sidebar-item">
-			<div class="col-md-8">
-				<h3>Live chat</h3>
+			<div class="col-xs-8 col-sm-12 col-md-8">
+				<h3>
+					<a id="livechat" class="anchor" href="#livechat" aria-hidden="true"></a>
+					Live chat
+				</h3>
 				<p>
 					For quick pointers<br>
 					Tuesday to Saturday<br>
 					09:00 to 17:00
 				</p>
 			</div>
-			<div class="col-md-4">
+			<div class="col-xs-4 col-sm-6 col-md-4">
 				<!-- BEGIN ProvideSupport.com Graphics Chat Button Code -->
-				<div id="ciEvtM"></div><div id="scEvtM"></div><div id="sdEvtM"></div>
-				<script type="text/javascript">
-					var seEvtM=document.createElement("script");seEvtM.type="text/javascript";var seEvtMs=(location.protocol.indexOf("https")==0?"https":"http")+"://image.providesupport.com/js/0or0ihkh4hylp1qnz87w7c01gr/safe-standard.js?ps_h=EvtM&ps_t="+new Date().getTime();setTimeout("seEvtM.src=seEvtMs;document.getElementById('sdEvtM').appendChild(seEvtM)",1)
-				</script>
-				<noscript>
-					<a href="http://www.providesupport.com?messenger=0or0ihkh4hylp1qnz87w7c01gr">Live Help Desk</a>
-				</noscript>
+				<div id="ciEvtM"></div><div id="scEvtM"></div><div id="sdEvtM"></div><script type="text/javascript">var seEvtM=document.createElement("script");seEvtM.type="text/javascript";var seEvtMs=(location.protocol.indexOf("https")==0?"https":"http")+"://image.providesupport.com/js/0or0ihkh4hylp1qnz87w7c01gr/safe-standard.js?ps_h=EvtM&ps_t="+new Date().getTime();setTimeout("seEvtM.src=seEvtMs;document.getElementById('sdEvtM').appendChild(seEvtM)",1)</script><noscript><a href="http://www.providesupport.com?messenger=0or0ihkh4hylp1qnz87w7c01gr">Live Help Desk</a></noscript>
 				<!-- END ProvideSupport.com Graphics Chat Button Code -->
 			</div>
 		</div>
 		<div class="phone sidebar-item">
-			<div class="col-md-8">
+			<div class="col-xs-8 col-sm-12 col-md-8">
 				<h3>Phone</h3>
 				<p>+44 (0) 20 8876 3444</p>
 			</div>
-			<div class="col-md-4">
+			<div class="col-xs-4 col-sm-6 col-md-4">
 				<p>
 					<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-phone.png" alt="Phone us" class="img-responsive">
 				</p>
 			</div>
 		</div>
 		<div class="postal sidebar-item">
-			<div class="col-md-8">
+			<div class="col-xs-8 col-sm-12 col-md-8">
 				<h3>Post</h3>
 				<p>
 					Please write to: <br>
 					The National Archives<br>
-					Kew, Richmond, Surrey, TW9 4DU
+					Kew, Richmond, Surrey,<br>
+					TW9 4DU
 				</p>
 			</div>
-			<div class="col-md-4">
+			<div class="col-xs-4 col-sm-6 col-md-4">
 				<p>
 					<img src="<?php echo str_replace( home_url(), '', get_template_directory_uri() ); ?>/img/icon-128-post.png" alt="Email us" class="img-responsive">
 				</p>
