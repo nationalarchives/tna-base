6d9ae1f5e7d8946b382f32226d44e3fcaceeea82
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-12-20 12:41:15 +0000

    Fix

--- js/compiled/tna-base.js
+++ js/compiled/tna-base.js
@@ -277,24 +277,9 @@ $.fn.webtrends_click_handler = function () {
     $.fn.newsletterBackToOrigin = function (options) {
         var settings = $.extend({}, $.fn.newsletterBackToOrigin.defaults, options);
         return this.each(function () {
-            var thankYouURL = "http://nationalarchives.gov.uk/about/get-involved/newsletters/thank-you/",
-                thankYouURLTestlb = "http://getinvolved.testlb.nationalarchives.gov.uk/newsletters/thank-you/",
-                thankYouURLTest = "http://test.nationalarchives.gov.uk/about/get-involved/newsletters/thank-you/",
-                thankYouURLDev = "http://tna-base:8888/thank-you/",
+            var thankYouURL = "http://test.nationalarchives.gov.uk/about/get-involved/newsletters/the-national-archives-newsletter/thank-you/",
                 newValue = "?oldurl=" + window.location.href;
-
-            if(window.location.href === "http://nationalarchives.gov.uk") {
                 return settings.$element.val(thankYouURL + newValue);
-            }
-            else if(window.location.href === "http://getinvolved.testlb.nationalarchives.gov.uk") {
-                return settings.$element.val(thankYouURLTestlb + newValue);
-            }
-            else if(window.location.href === "http://test.nationalarchives.gov.uk") {
-                return settings.$element.val(thankYouURLTest + newValue);
-            }
-            else {
-                return settings.$element.val(thankYouURLDev + newValue);
-            }
         });
     }
 
