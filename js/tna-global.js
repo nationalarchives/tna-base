009f9cd78d9084df6d1c9973a6dd9edd92033044
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2016-12-20 12:29:38 +0000

    Dev environment changes

--- js/tna-global.js
+++ js/tna-global.js
@@ -16,6 +16,7 @@
         return this.each(function () {
             var thankYouURL = "http://nationalarchives.gov.uk/about/get-involved/newsletters/thank-you/",
                 thankYouURLTestlb = "http://getinvolved.testlb.nationalarchives.gov.uk/newsletters/thank-you/",
+                thankYouURLTest = "http://test.nationalarchives.gov.uk/about/get-involved/newsletters/thank-you/",
                 thankYouURLDev = "http://tna-base:8888/thank-you/",
                 newValue = "?oldurl=" + window.location.href;
 
@@ -24,6 +25,9 @@
             }
             else if(window.location.href === "http://getinvolved.testlb.nationalarchives.gov.uk") {
                 return settings.$element.val(thankYouURLTestlb + newValue);
+            }
+            else if(window.location.href === "http://test.nationalarchives.gov.uk") {
+                return settings.$element.val(thankYouURLTest + newValue);
             }
             else {
                 return settings.$element.val(thankYouURLDev + newValue);
