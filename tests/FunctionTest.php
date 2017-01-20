0c05b40e2f59d57bf9f0af3e3af0682ed67b20af
Chris Bishop <domingobishop@gmail.com>
2017-01-20 15:54:47 +0000

    tna functions tests

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -7,4 +7,30 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(true);
         $this->assertFalse(false);
     }
+
+    // tna-functions.php
+    public function test_tna_styles()
+    {
+        $this->assertTrue(function_exists('tna_styles'));
+    }
+    public function test_tna_scripts()
+    {
+        $this->assertTrue(function_exists('tna_scripts'));
+    }
+    public function test_load_custom_wp_admin_style()
+    {
+        $this->assertTrue(function_exists('load_custom_wp_admin_style'));
+    }
+    public function test_first_sentence()
+    {
+        $this->assertTrue(function_exists('first_sentence'));
+    }
+    public function test_wpcodex_add_excerpt_support_for_pages()
+    {
+        $this->assertTrue(function_exists('wpcodex_add_excerpt_support_for_pages'));
+    }
+    public function test_get_query_string_newsletter_previous_url()
+    {
+        $this->assertTrue(function_exists('tna_styles'));
+    }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require dirname( __DIR__ ) . '/inc/functions/tna-globals.php';
+require dirname( __DIR__ ) . '/inc/functions/tna-functions.php';
 
 // Enable Composer autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
