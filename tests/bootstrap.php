edcb90483770e758ddcbeb663f1b219a2875e102
Chris Bishop <domingobishop@gmail.com>
2017-01-20 16:05:19 +0000

    inc 404 redirect

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 require dirname( __DIR__ ) . '/inc/functions/tna-globals.php';
 require dirname( __DIR__ ) . '/inc/functions/tna-functions.php';
+require dirname( __DIR__ ) . '/inc/functions/404-redirect.php';
 
 // Enable Composer autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
