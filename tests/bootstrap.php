c4fa0df89712b8183bee707ad3a7c55f6cf2f3af
Chris Bishop <domingobishop@gmail.com>
2017-01-20 16:18:34 +0000

    dimox test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 require dirname( __DIR__ ) . '/inc/functions/tna-globals.php';
 require dirname( __DIR__ ) . '/inc/functions/tna-functions.php';
 require dirname( __DIR__ ) . '/inc/functions/404-redirect.php';
+require dirname( __DIR__ ) . '/inc/functions/dimox_breadcrumbs.php';
 
 // Enable Composer autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
