c76a41e73b3e97ef99734f3a6d30c3c59e4e3139
Chris Bishop <domingobishop@gmail.com>
2017-01-20 16:03:37 +0000

    404 redirect test

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -8,29 +8,33 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(false);
     }
 
-    // tna-functions.php
-    public function test_tna_styles()
+    public function test_exists_tna_styles()
     {
         $this->assertTrue(function_exists('tna_styles'));
     }
-    public function test_tna_scripts()
+    public function test_exists_tna_scripts()
     {
         $this->assertTrue(function_exists('tna_scripts'));
     }
-    public function test_load_custom_wp_admin_style()
+    public function test_exists_load_custom_wp_admin_style()
     {
         $this->assertTrue(function_exists('load_custom_wp_admin_style'));
     }
-    public function test_first_sentence()
+    public function test_exists_first_sentence()
     {
         $this->assertTrue(function_exists('first_sentence'));
     }
-    public function test_wpcodex_add_excerpt_support_for_pages()
+    public function test_exists_wpcodex_add_excerpt_support_for_pages()
     {
         $this->assertTrue(function_exists('wpcodex_add_excerpt_support_for_pages'));
     }
-    public function test_get_query_string_newsletter_previous_url()
+    public function test_exists_get_query_string_newsletter_previous_url()
     {
         $this->assertTrue(function_exists('tna_styles'));
+    }
+
+    public function test_exists_redirect_if_404()
+    {
+        $this->assertTrue(function_exists('redirect_if_404'));
     }
 }
