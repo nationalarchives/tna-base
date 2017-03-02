621be13846115984d7ef9e7b90e1687d2ef9c004
Gwyn Jones <m@gwnjns.com>
2017-03-02 11:18:50 +0000

    Introduce test for .mobile-home-link presence

--- js/tests/MegaMenu/tests.js
+++ js/tests/MegaMenu/tests.js
@@ -17,22 +17,6 @@ QUnit.test("#mega-menu-pull-down toggles #nav", function (assert) {
 
 });
 
-QUnit.module("Promotional image");
-
-QUnit.test("The promotional image should NOT exist in the DOM before the plugin is applied", function (assert) {
-
-    assert.ok($("ul.sub-menu:last .imgContent").length == 0, 'The promotional image does not exist');
-
-});
-
-QUnit.test("The promotional image should exist in the DOM after the plugin is applied", function (assert) {
-
-    $("ul.sub-menu:last").append_promotional_image();
-
-    assert.ok($("ul.sub-menu:last .imgContent").length == 1, 'The promotional image does exist');
-
-});
-
 QUnit.module("Appending home links to mega menu");
 
 
@@ -53,6 +37,22 @@ QUnit.test("Every target element should have a mg-more link after the plugin is 
         assert.ok($(this).hasClass('mg-more'), 'All "mg-more" links are in the DOM');
     })
 
+});
+
+QUnit.test("The first link in every .sub-menu should have the class .mobile-home-link (unless it is the 'More...' link)", function (assert) {
+
+    $(".mega-menu > ul > li > a").append_home_links_to_mega_menu();
+
+    $('.sub-menu li:first-child').each(function () {
+
+        var $this = $(this);
+
+        // This is brittle but will suffice for the refactoring
+
+        if ($this.text() !== 'Bookshop') {
+            assert.ok($this.hasClass('mobile-home-link'), $this.text() + ' has the ".mobile-home-link" class');
+        }
+    })
 
 });
 
@@ -102,7 +102,6 @@ if ($(window).width() < 500) {
             }, 1000);
         });
 
-
     });
 }
 
@@ -114,5 +113,22 @@ QUnit.test("No links should have click handlers before the plugin is applied", f
     $('a').each(function () {
         assert.ok(typeof this.onclick !== 'function', "No links in the mega menu have click handlers");
     });
+
+});
+
+
+QUnit.module("Promotional image");
+
+QUnit.test("The promotional image should NOT exist in the DOM before the plugin is applied", function (assert) {
+
+    assert.ok($("ul.sub-menu:last .imgContent").length == 0, 'The promotional image does not exist');
+
+});
+
+QUnit.test("The promotional image should exist in the DOM after the plugin is applied", function (assert) {
+
+    $("ul.sub-menu:last").append_promotional_image();
+
+    assert.ok($("ul.sub-menu:last .imgContent").length == 1, 'The promotional image does exist');
 
 });
