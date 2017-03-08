cdf3fede70f4a469aba3e500e50a36fd4b492702
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2017-03-08 15:55:03 +0000

    Added hidden input name='cd_SOURCE', value='web' and aria-labelledby to newsletter for accessibility

--- inc/content/footer-newsletter.php
+++ inc/content/footer-newsletter.php
@@ -3,7 +3,7 @@
         <img src="http://www.nationalarchives.gov.uk/wp-content/themes/tna-base/img/tna-newsletter-icon.png" alt="TNA Newsletter Icon"/>
     </div>
 
-    <div class="news-content">
+    <div class="news-content" id="newsletterAccessibility">
         <h2>Send me The National Archives’ newsletter</h2>
         <p>A monthly round-up of news, blogs, offers and events</p>
     </div>
@@ -13,11 +13,12 @@
             <input type="hidden" name="addressbookid" value="636353"> <!-- homepage and general sign up -->
             <!-- input type="hidden" name="addressbookid" value="732466" --> <!-- first world war portal sign up -->
             <input type="hidden" name="userid" value="173459">
+            <input type="hidden" name="cd_SOURCE" value="web">
             <input type="hidden" name="ReturnURL"
                    value="http://www.nationalarchives.gov.uk/about/get-involved/newsletters/the-national-archives-newsletter/thank-you/">
             <label for="Email">Send me The National Archives’ newsletter</label>
             <input type="email" name="Email" id="Email" required="required"
-                   placeholder="Enter your email address">
+                   placeholder="Enter your email address" aria-labelledby="newsletterAccessibility">
             <input id="newsletterSignUp" type="submit" name="Submit" value="Subscribe" class="margin-left-medium">
         </form>
     </div>
