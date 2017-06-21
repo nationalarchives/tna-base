bd993e529c503c88261cfc2ba5a38203cc16c646
Chris Bishop <domingobishop@gmail.com>
2017-06-21 12:21:05 +0100

    removed pre_path from url rewrites

--- inc/functions/url-rewriting.php
+++ inc/functions/url-rewriting.php
@@ -9,18 +9,13 @@ function make_path_relative( $url ) {
     global $pre_path;
     return str_replace( site_url(), $pre_path, $url );
 }
-// Make template URLs relative without the $pre_path
-function make_path_relative_no_pre_path( $url ) {
-	return str_replace( site_url(), '', $url );
-}
 // Fix URLs in wp_head
 function tna_wp_head() {
     ob_start();
     wp_head();
     $wp_head = ob_get_contents();
     ob_end_clean();
-    global $pre_path;
-    $wp_head = str_replace( site_url(), 'http://www.nationalarchives.gov.uk' . $pre_path, $wp_head );
+    $wp_head = str_replace( site_url(), 'http://www.nationalarchives.gov.uk', $wp_head );
     echo $wp_head;
 }
 // Make content URLs relative
