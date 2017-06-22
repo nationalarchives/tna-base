a59afbf1a9c3918c9e3324f9437224f87abc9602
Chris Bishop <domingobishop@gmail.com>
2017-06-22 14:09:18 +0100

    removed pre_path from redirect_if_404

--- inc/functions/404-redirect.php
+++ inc/functions/404-redirect.php
@@ -1,16 +1,18 @@
 <?php
+
 // 404 Redirection function - called in header.php
 if ( ! function_exists( 'redirect_if_404' ) ) :
 	function redirect_if_404() {
 		if ( is_404() ) {
-			// Pre path for child sites
-			global $pre_path;
+
 			// Format string with placeholders for components
 			$scheme = ( isset($_SERVER['REQUEST_SCHEME']) ? $_SERVER['REQUEST_SCHEME'] : '' );
 			$requested_page = $scheme . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+
 			// Sanitizing the URL before use
 			$requested_page_safe = filter_var( $requested_page, FILTER_SANITIZE_URL );
-			$requested_page_safe = str_replace( site_url(), 'http://www.nationalarchives.gov.uk' . $pre_path, $requested_page_safe );
+			$requested_page_safe = str_replace( site_url(), 'http://www.nationalarchives.gov.uk', $requested_page_safe );
+
 			// Format string for redirection app URL with single placeholder
 			$redirect_app_url_format = "http://www.nationalarchives.gov.uk/PageNotFound/PageNotFound.aspx?url=%s";
 			$redirection_url         = sprintf( $redirect_app_url_format, $requested_page_safe );
