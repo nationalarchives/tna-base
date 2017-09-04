475e5287b57548dab38bd36f8a1868691911b3dd
Christopher Bishop <domingobishop@gmail.com>
2017-09-04 10:37:33 +0100

    refactored feature box for section landing template

--- inc/content/feature-box.php
+++ inc/content/feature-box.php
@@ -1,48 +1,29 @@
 <?php
 global $post;
-
-/* Feature box */
-$content_with_feat_box = '<div class="col-xs-12 col-sm-8 col-md-8">';
-$content_with_feat_img = '<div class="col-xs-12 col-sm-6 col-md-6">';
-$feat_box = get_post_meta(get_the_ID(), 'feat_box', true);
-
-if (!empty( $feat_box )) { // This is the custom field block
-	echo $content_with_feat_box;
-	if (have_posts()) :
-		while (have_posts()) :
-			the_post();
-			the_content();
-			echo '</div>';
-			echo '<div class="col-xs-12 col-sm-4 col-md-4"><div class="well">'.wpautop($feat_box).'</div></div>';
-		endwhile;
-	endif;
-} elseif (has_post_thumbnail()) { // This is the feature image block.
-	echo $content_with_feat_img;
-	if (have_posts()) :
-		while (have_posts()) :
-			the_post();
-			the_content();
-			echo '</div>';
-			echo '<div class="col-xs-12 col-sm-6 col-md-6">';
-			$image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'feature-box-thumb' );
-			?>
-			<figure class="feature-img">
-				<img src="<?php echo make_path_relative($image_url[0]); ?>" class="img-responsive" alt="<?php echo $post->post_title ?>">
-				<?php get_image_caption( 'bottom' ) ?>
-			</figure>
-			<?php
-			echo '</div>';
-		endwhile;
-	endif;
-} elseif (empty($feat_box) && the_post_thumbnail() == null){
-	echo $content_with_feat_box;
-	if (have_posts()) :
-		while (have_posts()) :
-			the_post();
-			the_content();
-		endwhile;
-	endif;
-	echo '</div>';
-	echo '<div class="col-xs-12 col-sm-4 col-md-4">&nbsp;</div>';
-}
+$feature_box    = get_post_meta( $post->ID, 'feat_box', true );
+$feature_image  = make_path_relative_no_pre_path( get_feature_image_url( 'feature-box-thumb' ) );
+$content_class  = ($feature_image) ? 'col-xs-12 col-sm-6 col-md-6' : 'col-xs-12 col-sm-8 col-md-8';
 ?>
+<article>
+    <div class="entry-header">
+        <h1><?php the_title(); ?></h1>
+    </div>
+    <div class="row entry-content">
+        <div class="<?php echo $content_class; ?>">
+            <?php the_content(); ?>
+        </div>
+        <?php if ($feature_box) { ?>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <div class="well">
+                    <?php echo wpautop($feature_box); ?>
+                </div>
+            </div>
+        <?php } elseif ($feature_image) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <figure class="feature-img">
+                    <img src="<?php echo $feature_image; ?>" class="img-responsive" alt="<?php the_title(); ?>">
+                </figure>
+            </div>
+        <?php } ?>
+    </div>
+</article>
--- page-section-landing.php
+++ page-section-landing.php
@@ -3,22 +3,20 @@
 Template Name: Section landing
 */
 get_header(); ?>
+
 <?php get_template_part( 'breadcrumb' ); ?>
+
 	<main id="primary" role="main" class="content-area">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<article>
-						<div class="entry-header">
-							<h1><?php the_title(); ?></h1>
-						</div>
-						<div class="row entry-content">
-							<?php get_template_part( 'inc/content/feature-box' ); ?>
-						</div>
-					</article>
+                    <?php while ( have_posts() ) : the_post();
+                        get_template_part( 'inc/content/feature-box' );
+                    endwhile; ?>
 				</div>
 			</div>
 			<?php get_template_part( 'inc/content/children-grandchildren-loop' ); ?>
 		</div>
 	</main>
+
 <?php get_footer(); ?>
