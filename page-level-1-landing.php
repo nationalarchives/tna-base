0ac03ff9e3f97076b0f719527b4eb8d4a2391ab7
Chris Bishop <domingobishop@gmail.com>
2017-09-08 15:46:19 +0100

    wpautop added

--- page-level-1-landing.php
+++ page-level-1-landing.php
@@ -16,7 +16,7 @@ get_header(); ?>
 					global $post;
 					$image      = make_path_relative_no_pre_path( get_feature_image_url( $post->ID, 'full-page-width', true ) );
 					$title      = get_the_title();
-					$content    = get_the_content();
+					$content    = wpautop(get_the_content());
 					$button     = get_post_meta( $post->ID, 'action_button_title', true );
 					$url        = get_post_meta( $post->ID, 'action_button_url', true );
 					get_page_banner( 'level one', $title, $image, $content, $button, $url );
@@ -32,7 +32,7 @@ get_header(); ?>
 						$boxes[$i]['title']   = get_post_meta( $post->ID, 'box_title_' . $i, true );
 						$boxes[$i]['url']     = get_post_meta( $post->ID, 'box_title_url_' . $i, true );
 						$boxes[$i]['image']   = get_post_meta( $post->ID, 'box_image_url_' . $i, true );
-						$boxes[$i]['content'] = get_post_meta( $post->ID, 'box_content_' . $i, true );
+						$boxes[$i]['content'] = wpautop(get_post_meta( $post->ID, 'box_content_' . $i, true ));
 						$boxes[$i]['display'] = get_post_meta( $post->ID, 'box_width_' . $i, true );
 					}
 					get_content_meta_boxes( $boxes );
