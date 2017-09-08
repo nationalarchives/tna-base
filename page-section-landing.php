29b2b0a57503beb51d932d4402db22887d737be9
Chris Bishop <domingobishop@gmail.com>
2017-09-08 17:09:59 +0100

    wpautop on section landing

--- page-section-landing.php
+++ page-section-landing.php
@@ -15,7 +15,7 @@ get_header(); ?>
 						global $post;
 						$image           = make_path_relative_no_pre_path( get_feature_image_url( $post->ID, 'feature-box-thumb' ) );
 						$title           = get_the_title();
-						$content         = get_the_content();
+						$content         = wpautop(get_the_content());
 						$section_content = wpautop(get_post_meta( $post->ID, 'feat_box', true ));
 						get_page_banner( 'section', $title, $image, $content, '', '', $section_content );
 						?>
