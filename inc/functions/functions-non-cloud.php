28ec73de6837885d82a25891e820b59e9cc47d3e
Chris Bishop <domingobishop@gmail.com>
2017-09-11 15:17:36 +0100

    TNA_CLOUD condition on url rewrites

--- inc/functions/functions-non-cloud.php
+++ inc/functions/functions-non-cloud.php
@@ -6,17 +6,29 @@
  */
 
 // Make styles and scripts paths relative
-function tna_styles_scripts_relative( $url ) {
-	return str_replace( site_url(), '', $url );
+function tna_styles_scripts_relative( $content ) {
+	if (!TNA_CLOUD) {
+		return str_replace( site_url(), '', $content );
+	} else {
+		return $content;
+	}
 }
 // Make template URLs relative
 function make_path_relative( $url ) {
-	global $pre_path;
-	return str_replace( site_url(), $pre_path, $url );
+	if (!TNA_CLOUD) {
+		global $pre_path;
+		return str_replace( site_url(), $pre_path, $url );
+	} else {
+		return $url;
+	}
 }
 // Make template URLs relative without the $pre_path
 function make_path_relative_no_pre_path( $url ) {
-	return str_replace( site_url(), '', $url );
+	if (!TNA_CLOUD) {
+		return str_replace( site_url(), '', $url );
+	} else {
+		return $url;
+	}
 }
 // Fix URLs in wp_head
 function tna_wp_head() {
@@ -25,7 +37,10 @@ function tna_wp_head() {
 	$wp_head = ob_get_contents();
 	ob_end_clean();
 	global $pre_path;
-	$wp_head = str_replace( site_url(), 'http://www.nationalarchives.gov.uk' . $pre_path, $wp_head );
+	if (!TNA_CLOUD) {
+		$protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
+		$wp_head = str_replace( site_url(), $protocol.'://www.nationalarchives.gov.uk' . $pre_path, $wp_head );
+	}
 	echo $wp_head;
 }
 // Make content URLs relative
