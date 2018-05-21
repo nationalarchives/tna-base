a2ad238060adf1dbc018cc8b11cc8d0902cdefec
SDWilked <simonwilkes@gmail.com>
2018-05-21 11:25:55 +0100

    Adding link to Privacy Policy under newsletter sign-up form

--- partials/footer-newsletter.php
+++ partials/footer-newsletter.php
@@ -21,5 +21,8 @@
                    placeholder="Enter your email address" aria-labelledby="newsletterAccessibility">
             <input id="newsletterSignUp" type="submit" name="Submit" value="Subscribe" class="margin-left-medium">
         </form>
+        <div class="small small-margin"><a href="http://www.nationalarchives.gov.uk/legal/privacy-policy/"
+                                           title="Please read our privacy policy">Please read our
+                privacy policy</a></div>
     </div>
 </div>
