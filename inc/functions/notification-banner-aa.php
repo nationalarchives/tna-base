b7a1ac9f52def3a61c31f495b2c95f6338873139
Punal Chotrani <punalsc@gmail.com>
2018-10-04 16:00:32 +0100

    Updated url

--- inc/functions/notification-banner-aa.php
+++ inc/functions/notification-banner-aa.php
@@ -10,7 +10,7 @@ if ( ! function_exists( 'notification_banner' ) ) {
                         <div class="col-md-12">
                             <div class="notice">
                                 <strong class="title">PARTICIPATE</strong>
-                                Enter a prize draw by <a href="" target="_blank">answering 4 questions</a> about your online visit.
+                                Enter a prize draw by <a href="https://www.smartsurvey.co.uk/s/XEM2T/" target="_blank">answering 4 questions</a> about your online visit.
                             </div>
                         </div>
                     </div>
