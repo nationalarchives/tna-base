433ff2edbeeabf0a9979d8cb160cc406e111b12a
Chris Bishop <domingobishop@gmail.com>
2018-10-18 13:52:37 +0100

    remove remove action

--- functions.php
+++ functions.php
@@ -83,7 +83,6 @@ remove_action( 'wp_head', 'feed_links', 2 );
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wlwmanifest_link' );
-remove_action( 'wp_head', 'rsd_link' );
 
 // Remove REST API
 remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
