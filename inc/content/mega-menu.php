55c90a89d50b0a0c21a64e9bfb5a7e099030593f
domingobishop <domingobishop@gmail.com>
2019-04-08 14:46:32 +0100

    static megamenu fallback

--- inc/content/mega-menu.php
+++ inc/content/mega-menu.php
@@ -1,82 +1,61 @@
 <!-- NORMAL mega menu -->
-    <div class="row">
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-                <h3><a onClick="dcsMultiTrack('DCS.dcsuri','/menu/About us ','WT.ti','Menu - About us ');" href="/about">About us</a></h3>
-                <ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/About us ','WT.ti','Menu - About us ');" href="/about/">About us home</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Our role ','WT.ti','Menu - Our role ');" href="/about/our-role/">Our role</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Visit us ','WT.ti','Menu - Visit us ');" href="/about/visit-us/">Visit us</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Opening times ','WT.ti','Menu - Opening times ');" href="/about/visit-us/opening-times">Opening times</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Jobs ','WT.ti','Menu - Jobs ');" href="/about/jobs/">Jobs</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Get involved ','WT.ti','Menu - Get involved ');" href="/about/get-involved/">Get involved</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/News ','WT.ti','Menu - News ');" href="/about/news/">News</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Press room ','WT.ti','Menu - Press room ');" href="/about/press-room/">Press room</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Commercial opportunities ','WT.ti','Menu - Commercial opportunities ');" href="/about/commercial-opportunities/">Commercial opportunities</a></li>
-                </ul>
-        </div>
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-					<h3><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Education ','WT.ti','Menu - Education ');" href="/education/">Education</a></h3>
-					<ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Education ','WT.ti','Menu - Education ');" href="/education">Education home</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Topics ','WT.ti','Menu - Topics ');" href="/education/sessions-and-resources/?time-period=medieval,early-modern,empire-and-industry,victorians,early-20th-century,interwar,second-world-war,postwar">Time periods</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Lessons ','WT.ti','Menu - Lessons ');" href="/education/sessions-and-resources/?resource-type=lesson">Lessons</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Workshops ','WT.ti','Menu - Workshops ');" href="/education/sessions-and-resources/?resource-type=workshop">Workshops</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Videoconferences ','WT.ti','Menu - Videoconferences ');" href="/education/sessions-and-resources/?resource-type=video-conferences">Videoconferences</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Virtual classroom ','WT.ti','Menu - Virtual classroom ');" href="/education/sessions-and-resources/?resource-type=virtual-classroom">Virtual classroom</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Professional development ','WT.ti','Menu - Professional development ');" href="/education/teachers/professional-development/">Professional development</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/For teachers ','WT.ti','Menu - For teachers ');" href="/education/teachers/">For teachers</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/For students ','WT.ti','Menu - For students ');" href="/education/students/">For students</a></li>
-					</ul>
-				</div>
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-					<h3><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/help-with-your-research ','WT.ti','Menu – Help with your research home ');" href="/help-with-your-research">Help with your research</a></h3>
-					<ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/help-with-your-research ','WT.ti','Menu – Help with your research home ');" href="/help-with-your-research/">Help with your research home</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Search Discovery, our catalogue ','WT.ti','Menu - Search Discovery, our catalogue ');" href="http://discovery.nationalarchives.gov.uk/">Search Discovery, our catalogue</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Start your research here ','WT.ti','Menu - Start your research here ');" href="/help-with-your-research/start-here/">Start your research here</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/help-with-your-research/research-guides-keywords/ ','WT.ti','Menu - Research guides A-Z ');" href="/help-with-your-research/research-guides-keywords/">Research guides A-Z</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/online-collections ','WT.ti','Menu - Online collections ');" href="/help-with-your-research/research-guides/?research-category=online">Online collections</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/UK Government Web Archive ','WT.ti','Menu - UK Government Web Archive ');" href="/webarchive/">UK Government Web Archive</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Certificates of British Citizenship ','WT.ti','Menu - Certificates of British Citizenship ');" href="/help-with-your-research/research-guides/certificates-of-british-citizenship-1949-1986/">Certificates of British Citizenship</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Record copying ','WT.ti','Menu - Record copying ');" href="/help-with-your-research/record-copying/">Record copying</a></li>
-					</ul>
-        </div>
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-					<h3 class=hyphenate><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Information Management ','WT.ti','Menu - Information Management ');" href="/information-management/">Information management</a></h3>
-					<ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Information Management ','WT.ti','Menu - Information Management ');" href="/information-management/">Information management home</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Guidance and standards A-Z ','WT.ti','Menu - Guidance and standards A-Z ');" href="/information-management/browse-guidance-standards/">Guidance and standards A-Z</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Crown copyright ','WT.ti','Menu - Crown copyright ');" href="/information-management/re-using-public-sector-information/copyright/">Copyright</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Re-using PSI ','WT.ti','Menu - Re-using PSI ');" href=" /information-management/manage-information/">How to manage your information</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Re-using PSI ','WT.ti','Menu - Re-using PSI ');" href="/information-management/re-using-public-sector-information/">Re-using PSI</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Official publishing ','WT.ti','Menu - Official publishing ');" href="/information-management/producing-official-publications/">Producing official publications</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Records selection and transfer ','WT.ti','Menu - Records selection and transfer ');" href="/information-management/legislation/">Legislation and regulations</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Training ','WT.ti','Menu - Training ');" href="/information-management/training/">Training</a></li>
-					</ul>
-				</div>
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-					<h3><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Archives sector ','WT.ti','Menu - Archives sector ');" href="/archives-sector">Archives sector</a></h3>
-					<ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Archives sector ','WT.ti','Menu - Archives sector ');" href="/archives-sector">Archives sector home</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Historical Manuscripts Commission ','WT.ti','Menu - Historical Manuscripts Commission ');" href="/archives-sector/our-archives-sector-role/historical-manuscripts-commission/">Historical Manuscripts Commission</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Finding funding ','WT.ti','Menu - Finding funding ');" href="/archives-sector/finding-funding/">Finding funding</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Projects and programmes ','WT.ti','Menu - Projects and programmes ');" href="/archives-sector/projects-and-programmes/">Projects and programmes</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Case studies and research ','WT.ti','Menu - Case studies and research ');" href="/archives-sector/case-studies-and-research-reports/">Case studies and research reports</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Operating your archive ','WT.ti','Menu - Operating your archive ');" href="/archives-sector/advice-and-guidance/">Operating your archive</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Our role in the archives sector ','WT.ti','Menu - Our role in the archives sector ');" href="/archives-sector/our-archives-sector-role/">Our role in the archives sector</a></li>
-					</ul>
-				</div>
-        <div class="col-xxs-12 col-xs-6 col-sm-6 col-md-2">
-					<h3>More...</h3>
-					<ul>
-						<li class="mobileOnly"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/More? ','WT.ti','Menu - More? ');" href="/about/">More...</a></li>
-						<li class="image"><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Bookshop ','WT.ti','Menu - Bookshop ');" href="/bookshop/">Bookshop</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Image library ','WT.ti','Menu - Image library ');" href="/imagelibrary/">Image library</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Blog ','WT.ti','Menu - Blog ');" href="/blog/">Blog</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Podcasts and videos ','WT.ti','Menu - Podcasts and videos ');" href="/media/">Podcasts and videos</a></li>
-						<li><a onclick="dcsMultiTrack('DCS.dcsuri','/menu/Contact us ','WT.ti','Menu - Contact us ');" href="/contact/">Contact us</a></li>
-						<li class="imgContent"><a href="/first-world-war/" title="First World War 100 - read about our centenary programme"><img src="/images/home/menu-first-world-war-b.jpg" alt="Explore First World War 100"></a></li>
-                </ul>
-        </div>
-    </div>
+<ul class="main-ul"><li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about" class="toggle-sub-menu">About us</a>
+        <ul class="sub-menu"><li class="mobile-home-link"><a href="http://www.nationalarchives.gov.uk/about">About us home</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/our-role/">Our role</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/visit-us/">Visit us</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/visit-us/opening-times/">Opening times</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/visit-us/whats-on/">What’s on</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/visit-us/venue-hire/">Venue hire</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/jobs/">Jobs</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/get-involved/">Get involved</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/news/">News</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/about/commercial-opportunities/">Commercial opportunities</a></li>
+        </ul></li>
+    <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/" class="toggle-sub-menu">Education</a>
+        <ul class="sub-menu"><li class="mobile-home-link"><a href="http://www.nationalarchives.gov.uk/education/">Education home</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/sessions-and-resources/?time-period=medieval,early-modern,empire-and-industry,victorians,early-20th-century,interwar,second-world-war,postwar">Time periods</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/sessions-and-resources/?resource-type=lesson">Lessons</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/sessions-and-resources/?resource-type=workshop">Workshops</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/sessions-and-resources/?resource-type=video-conferences">Videoconferences</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/sessions-and-resources/?resource-type=virtual-classroom">Virtual classroom</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/teachers/professional-development/">Professional development</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/teachers/">For teachers</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/education/students/">For students</a></li>
+        </ul></li>
+    <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/help-with-your-research" class="toggle-sub-menu">Help with your research</a>
+        <ul class="sub-menu"><li class="mobile-home-link"><a href="http://www.nationalarchives.gov.uk/help-with-your-research">Help with your research home</a></li>
+            <li><a data-gtm="mega-menu" href="http://discovery.nationalarchives.gov.uk/">Search Discovery, our catalogue</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/help-with-your-research/start-here/">Start your research here</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/help-with-your-research/research-guides-keywords/">Research guides A-Z</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/help-with-your-research/research-guides/?research-category=online">Online collections</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/webarchive/">UK Government Web Archive</a></li>
+            <li><a data-gtm="mega-menu" href="https://www.nationalarchives.gov.uk/contact-us/british-citizenship-services/">British citizenship services</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/help-with-your-research/record-copying/">Record copying</a></li>
+        </ul></li>
+    <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/" class="toggle-sub-menu">Information management</a>
+        <ul class="sub-menu"><li class="mobile-home-link"><a href="http://www.nationalarchives.gov.uk/information-management/">Information management home</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/browse-guidance-standards/">Guidance and standards A-Z</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/">Copyright</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/manage-information/">How to manage your information</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/">Re-using PSI</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/producing-official-publications/">Producing official publications</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/legislation/">Legislation and regulations</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/information-management/training/">Training</a></li>
+        </ul></li>
+    <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector" class="toggle-sub-menu">Archives sector</a>
+        <ul class="sub-menu"><li class="mobile-home-link"><a href="http://www.nationalarchives.gov.uk/archives-sector">Archives sector home</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/our-archives-sector-role/historical-manuscripts-commission/">Historical Manuscripts Commission</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/finding-funding/">Finding funding</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/projects-and-programmes/">Projects and programmes</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/case-studies-and-research-reports/">Case studies and research reports</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/advice-and-guidance/">Operating your archive</a></li>
+            <li><a data-gtm="mega-menu" href="http://www.nationalarchives.gov.uk/archives-sector/our-archives-sector-role/">Our role in the archives sector</a></li>
+        </ul></li>
+    <li><div class="toggle-sub-menu" id="more-link">More…</div>
+        <ul class="sub-menu"><li><a data-gtm="mega-menu" href="http://bookshop.nationalarchives.gov.uk/">Bookshop</a></li>
+            <li><a data-gtm="mega-menu" href="https://images.nationalarchives.gov.uk/">Image library</a></li>
+            <li><a data-gtm="mega-menu" href="https://blog.nationalarchives.gov.uk/">Blog</a></li>
+            <li><a data-gtm="mega-menu" href="https://media.nationalarchives.gov.uk/">Podcasts and videos</a></li>
+            <li><a data-gtm="mega-menu" href="https://www.nationalarchives.gov.uk/contact-us/">Contact us</a></li></ul></li>
+</ul>
 <!-- end mega-menu -->
