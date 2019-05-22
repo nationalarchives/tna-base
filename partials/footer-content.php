1c12c9305c696006d54fa440780299110856b4d8
ARBaynes <annabel.baynes@gmail.com>
2019-05-22 11:48:53 +0100

    Used find-and-replace on http://bookshop.nationalarchives
    Replaced with https://bookshop.nationalarchives.

--- partials/footer-content.php
+++ partials/footer-content.php
@@ -65,8 +65,8 @@
             <div class="footer-col col-xs-12 col-sm-6 col-md-6">
                 <h4>Websites</h4>
                 <ul>
-                    <li><a href="https://blog.nationalarchives.gov.uk/">Blog</a></li>
-                    <li><a href="https://media.nationalarchives.gov.uk/">Podcasts and videos</a></li>
+                    <li><a href="http://blog.nationalarchives.gov.uk/">Blog</a></li>
+                    <li><a href="http://media.nationalarchives.gov.uk/">Podcasts and videos</a></li>
                     <li><a href="https://bookshop.nationalarchives.gov.uk/">Bookshop</a></li>
                     <li><a href="https://images.nationalarchives.gov.uk/">Image library</a></li>
                     <li><a href="/webarchive/">UK Government Web Archive</a></li>
