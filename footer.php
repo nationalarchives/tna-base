a3203046e0c3674a0d7f7ff0547122dd6002bc95
domingobishop <domingobishop@gmail.com>
2019-08-06 15:15:37 +0100

    icons and newsletter signup

--- footer.php
+++ footer.php
@@ -1,14 +1,11 @@
 <a title="Go back to top" href="#" id="goTop"></a>
-<?php
-	global $post;
-
-	if (!has_category('hide-newsletter',$post->ID)) : ?>
-			<div class="container">
-				<?php get_template_part( 'partials/footer-newsletter' );?>
-			</div>
-	<?php endif;
-?>
-<footer id="footer" class="breather-top-bottom" role="contentinfo">
+<footer id="footer" role="contentinfo">
+    <?php
+    global $post;
+    if (!has_category('hide-newsletter',$post->ID)) {
+        get_template_part('partials/footer-newsletter');
+    }
+    ?>
 	<div class="container">
 		<?php get_template_part( 'partials/footer-content' ); ?>
 	</div>
