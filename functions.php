9329f93a1373eb1fa9b05fc98fc31671eba1d002
domingobishop <domingobishop@gmail.com>
2019-08-09 17:42:21 +0100

    get_meta_og_on_save

--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ add_action( 'init', 'notification_meta_boxes' );
 add_action( 'admin_menu', 'tna_base_menu' );
 add_action( 'init', 'portal_landing_meta_boxes' );
 add_action( 'save_post', 'portal_landing_get_og_meta_on_save' );
+add_action( 'save_post', 'get_meta_og_on_save' );
 
 // add_filter
 add_filter( 'document_title_parts', 'title_tag', 10, 1 );
