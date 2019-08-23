fa987f9f18c27eb3aeb18d199939031b1f72a4de
domingobishop <domingobishop@gmail.com>
2019-08-23 17:34:44 +0100

    amend card labels

--- inc/functions-level-one.php
+++ inc/functions-level-one.php
@@ -126,7 +126,7 @@ function level_one_meta_boxes() {
                     'desc' => '',
                     'id' => 'card_level_one_label_'.$i,
                     'type' => 'select',
-                    'options' => array('Please select a label', 'About', 'Audio', 'Blog', 'Careers', 'Case studies', 'Collaboration', 'Event', 'Feature', 'Guidance', 'Multimedia', 'News', 'Podcast', 'Projects', 'Resources', 'Services', 'Study resources', 'Training', 'Video', 'Webinar'),
+                    'options' => array('Please select a label', 'About', 'Audio', 'Blog', 'Careers', 'Case study', 'Collaboration', 'Event', 'Feature', 'Guidance', 'Multimedia', 'News', 'Podcast', 'Project', 'Resource', 'Service', 'Study resource', 'Training', 'Video', 'Webinar'),
                 ),
                 array(
                     'name' => 'Excerpt',
