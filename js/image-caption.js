cfc0341e0c7a75302d5a4d4c71cdc8a5f4f831bb
JamesWChan <jwchan1993@gmail.com>
2020-03-17 09:47:57 +0000

    Add try/catch block

--- js/image-caption.js
+++ js/image-caption.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
     if($(".image_caption_back").length){
-        var $imageCaption = $(".image_caption_back");
-        var ariaExpanded = $.parseJSON($(".eye_caption").attr("aria-expanded").toLowerCase());
-        var ariaHidden = $.parseJSON($imageCaption.attr("aria-hidden").toLowerCase());
+        try {
+            var $imageCaption = $(".image_caption_back");
+            var ariaExpanded = $.parseJSON($(".eye_caption").attr("aria-expanded").toLowerCase());
+            var ariaHidden = $.parseJSON($imageCaption.attr("aria-hidden").toLowerCase());
 
-        $(".eye_caption").on('click', function(){
-            $imageCaption.toggle();
-            ariaHidden = !ariaHidden;
-            ariaExpanded = !ariaExpanded;
-            $imageCaption.attr('aria-hidden', ariaHidden);
-            $(this).attr('aria-expanded', ariaExpanded);
-        })
+            $(".eye_caption").on('click', function(){
+                $imageCaption.toggle();
+                ariaHidden = !ariaHidden;
+                ariaExpanded = !ariaExpanded;
+                $imageCaption.attr('aria-hidden', ariaHidden);
+                $(this).attr('aria-expanded', ariaExpanded);
+            })
+        }
+        catch (e) {
+            return false;
+        }
     }
 });
