a66dec144c8cd55c8b210a77b742f6cdb50246fd
Andy Aldridge <andrew.aldridge@nationalarchives.gov.uk>
2021-01-05 12:19:32 +0000

    remove redundant tests

--- tests/TNAGlobalsTest.php
+++ tests/TNAGlobalsTest.php
@@ -12,18 +12,6 @@ class TNAGlobalsTest extends PHPUnit_Framework_TestCase
         $this->assertFalse(served_from_local_machine('127.0.0.1', '127.0.0.2', 'When passed non-identical arguments, false is returned'));
     }
 
-    public function test_path_to_mega_menu_local()
-    {
-        set_path_to_mega_menu(true);
-        $this->assertEquals(PATH_TO_MEGA_MENU_HTML, 'output.html');
-    }
-
-    public function test_path_to_mega_menu_remote()
-    {
-        set_path_to_mega_menu(false);
-        $this->assertEquals(PATH_TO_MEGA_MENU_HTML, 'D:/webapps/phpapps/mega-menu-feed-processor/output.html');
-    }
-
     public function test_check_for_specific_url_path_exists()
     {
         $this->assertTrue(function_exists('check_for_specific_url_path'));
