f9c16dce2c26c6f4e9970c8595b9bacc7af3a98a
Andy Aldridge <andrew.aldridge@nationalarchives.gov.uk>
2021-01-05 11:25:35 +0000

    Removed function and call to retrieve mega menu

--- inc/functions-non-cloud.php
+++ inc/functions-non-cloud.php
@@ -71,18 +71,6 @@ function served_from_local_machine($server_ip, $remote_ip)
 	return ($server_ip === $remote_ip);
 }
 
-/**
- * @param $development_machine - boolean obtained from calling served_from_local_machine()
- */
-function set_path_to_mega_menu($development_machine)
-{
-	if ($development_machine === true) {
-		define("PATH_TO_MEGA_MENU_HTML", 'output.html');
-	} else {
-		define("PATH_TO_MEGA_MENU_HTML", 'D:/webapps/phpapps/mega-menu-feed-processor/output.html');
-	}
-}
-
 function check_for_specific_url_path($url_path = '') {
 	$protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
 	$url = $protocol.'://nationalarchives.gov.uk';
