c263ac543def9c37404b5c8428de6ede21d8b780
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-03-22 16:43:49 +0000

    Add new global header and footer

--- footer.php
+++ footer.php
@@ -17,6 +17,15 @@
 
 <?php wp_footer(); ?>
 
+<script>
+	if(window.TNAFrontend && window.TNAFrontend.initAll) {
+		window.TNAFrontend.initAll()
+	}
+	if(window.TNAFrontendAnalytics && window.TNAFrontendAnalytics.GA4) {
+		new TNAFrontendAnalytics.GA4({ addTrackingCode: false })
+	}
+</script>
+
 </body>
 
 </html>
