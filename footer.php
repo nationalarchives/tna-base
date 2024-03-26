70e3cd07ce1a455c3e38ea54c3b34d03d431f726
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-03-26 12:42:27 +0000

    Update footer HTML

--- footer.php
+++ footer.php
@@ -1,19 +1,4 @@
-<footer id="footer" role="contentinfo">
-    <h2 class="sr-only">Footer</h2>
-    <?php
-    global $post;
-    if ( is_object($post) ) {
-        if (!has_category('hide-newsletter', $post->ID)) {
-            get_template_part('partials/footer-newsletter');
-        }
-    }
-    ?>
-    <div class="footer-content">
-        <div class="container">
-            <?php get_template_part( 'partials/footer-content' ); ?>
-        </div>
-    </div>
-</footer>
+<?php get_template_part( 'partials/footer-content' ); ?>
 
 <?php wp_footer(); ?>
 
